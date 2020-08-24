02f11be30ce2b63d051c3c1102618c595d1d2e6f
Josh Kapple <jkapple@concentricsky.com>
2020-08-24 12:51:56 -0700

    OSMT-228 - Update dev-stack to use MySQL 8, use utf8mb3 as collation for string fields

--- api/src/main/kotlin/edu/wgu/osmt/auditlog/AuditLogTable.kt
+++ api/src/main/kotlin/edu/wgu/osmt/auditlog/AuditLogTable.kt
@@ -1,5 +1,6 @@
 package edu.wgu.osmt.auditlog
 
+import edu.wgu.osmt.db.BaseTable
 import edu.wgu.osmt.db.TableWithInsertMapper
 import org.jetbrains.exposed.dao.id.LongIdTable
 import org.jetbrains.exposed.sql.`java-time`.datetime
@@ -8,11 +9,11 @@ import org.jetbrains.exposed.sql.statements.InsertStatement
 object AuditLogTable : TableWithInsertMapper<AuditLog>, LongIdTable("AuditLog") {
     override val table = this
     override val creationDate = datetime("creationDate")
-    val user = varchar("user", 256)
-    val operationType = varchar("operationType", 128)
-    val targetTableName = varchar("tableName", 128)
+    val user = varchar("user", 256, BaseTable.defaultCollation)
+    val operationType = varchar("operationType", 128, BaseTable.defaultCollation)
+    val targetTableName = varchar("tableName", 128, BaseTable.defaultCollation)
     val entityId = long("entityId")
-    val changedFields = text("changedFields")
+    val changedFields = text("changedFields", BaseTable.defaultCollation)
 
     override fun insertStatementApplyFromT(insertStatement: InsertStatement<Number>, t: AuditLog) {
         super.insertStatementApplyFromT(insertStatement, t)

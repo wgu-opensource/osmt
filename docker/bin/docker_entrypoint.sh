593ed66bc2f7c63f5b6cf373f3bc4df72583f4f2
Wiggins <wiggins@concentricsky.com>
2020-09-01 10:47:29 -0700

    cleanup argument logic in docker_entrypoint.sh

--- docker/bin/docker_entrypoint.sh
+++ docker/bin/docker_entrypoint.sh
@@ -7,20 +7,23 @@ cd ${BASE_DIR} || exit
 MISSING_ARGS=0
 
 if [[ -z ${ENVIRONMENT} ]]; then
-  "${MISSING_ARGS} 'ENVIRONMENT'"
+  MISSING_ARGS=$((MISSING_ARGS + 1))
+  echo "Missing environment 'ENVIRONMENT'"
 fi
 
-if [[ -z ${MYSQL_DB_URI} ]]; then
-  "${MISSING_ARGS} 'MYSQL_DB_URI'"
-  exit 128
+if [[ -z "${MYSQL_DB_URI}" ]]; then
+  MISSING_ARGS=$((MISSING_ARGS + 1))
+  echo "Missing environment 'MYSQL_DB_URI'"
 fi
 
-if [[ -z ${REDIS_URL} ]]; then
-  "${MISSING_ARGS} 'REDIS_URL'"
+if [[ -z "${REDIS_URI}" ]]; then
+  MISSING_ARGS=$((MISSING_ARGS + 1))
+  echo "Missing environment 'REDIS_URI'"
 fi
 
-if [[ -z ${ELASTICSEARCH_URI} ]]; then
-  "${MISSING_ARGS} 'ELASTICSEARCH_URI'"
+if [[ -z "${ELASTICSEARCH_URI}" ]]; then
+  MISSING_ARGS=$((MISSING_ARGS + 1))
+  echo "Missing environment 'ELASTICSEARCH_URI'"
 fi
 
 if [[ ${MISSING_ARGS} != 0 ]]; then
@@ -29,7 +32,7 @@ if [[ ${MISSING_ARGS} != 0 ]]; then
 else
   JAVA_CMD="/bin/java
               -Dspring.profiles.active=${ENVIRONMENT}
-              -Dspring.redis.url=${REDIS_URL}
+              -Dredis.uri=${REDIS_URI}
               -Ddb.uri=${MYSQL_DB_URI}
               -Des.uri=${ELASTICSEARCH_URI}
               -jar ${BASE_DIR}/bin/osmt.jar"

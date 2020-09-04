0e37e6b82ab303fdf152b1c11c65e3d1d2ef5cec
jcleary <jcleary@concentricsky.com>
2020-09-04 15:45:34 -0700

    [OSMT-269] Changed how job codes are inferred. Currently, categories in the code are expected not to equal zero if their part is "present"

--- ui/src/app/richskill/detail/rich-skill.component.ts
+++ ui/src/app/richskill/detail/rich-skill.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from "@angular/core"
 import { RichSkill } from "../RichSkill"
 import { RichSkillService } from "../service/rich-skill.service"
 import { ActivatedRoute } from "@angular/router"
-import { JobcodeParser, JobCodeType } from "../../jobcode/JobcodeParser"
+import {JobCodeBreakout, IJobCode} from "../../jobcode/Jobcode"
 
 @Component({
   selector: "app-richskill",
@@ -14,6 +14,13 @@ export class RichSkillComponent implements OnInit {
   uuidParam: string | null
   richSkill: RichSkill | null = null
   loading = true
+
+  majorCodes: string[] | null = null
+  minorCodes: string[] | null = null
+  broadCodes: string[] | null = null
+  detailedCodes: string[] | null = null
+  jobRoleCodes: string[] | null = null
+
 
   constructor(private richSkillService: RichSkillService, private route: ActivatedRoute) {
     this.route.params.subscribe(params => console.log(params))
@@ -31,41 +38,60 @@ export class RichSkillComponent implements OnInit {
     this.richSkillService.getSkillByUUID(uuid).subscribe(
       skill => {
         this.richSkill = skill
+        this.inferCodes()
         this.loading = false
       },
       (error) => {
         console.log(`Error loading skill: ${error}`)
         this.loading = false
       }
-    )
+
+    )  }
+
+  inferCodes(): void {
+    const occupations = this.richSkill?.occupations
+
+    if (occupations) {
+
+      const codeBreakouts: JobCodeBreakout[] = occupations
+        .map((code: IJobCode) => new JobCodeBreakout(code.code))
+
+      this.majorCodes = this.inferMajorCodes(codeBreakouts)
+      this.minorCodes = this.inferMinorCodes(codeBreakouts)
+      this.broadCodes = this.inferBroadCodes(codeBreakouts)
+      this.detailedCodes = this.inferDetailedCodes(codeBreakouts)
+      this.jobRoleCodes = this.inferJobRoleCodes(codeBreakouts)
+    }
   }
 
-  getJobCodes(type: JobCodeType): string[] {
-    const parser = new JobcodeParser()
-    return this.richSkill?.occupations
-        .map(({code}) => code)
-        .filter(code => parser.parseCode(code) === type)
-      || []
+  private inferMajorCodes(codeBreakouts: JobCodeBreakout[]): string[] | null {
+    const codes = codeBreakouts.flatMap(code => !!code?.majorCode() ? [code.majorCode() as string] : [])
+    return this.dedupeCodes(codes)
   }
 
-  getMajorJobCodes(): string[] {
-    return this.getJobCodes(JobCodeType.Major)
+  private inferMinorCodes(codeBreakouts: JobCodeBreakout[]): string[] | null {
+    const codes = codeBreakouts.flatMap(code => !!code?.minorCode() ? [code.minorCode() as string] : [])
+    return this.dedupeCodes(codes)
   }
 
-  getMinorJobCodes(): string[] {
-    return this.getJobCodes(JobCodeType.Minor)
+  private inferBroadCodes(codeBreakouts: JobCodeBreakout[]): string[] | null {
+    const codes = codeBreakouts.flatMap(code => !!code?.broadCode() ? [code.broadCode() as string] : [])
+    return this.dedupeCodes(codes)
   }
 
-  getBroadJobCodes(): string[] {
-    return this.getJobCodes(JobCodeType.Broad)
+  private inferDetailedCodes(codeBreakouts: JobCodeBreakout[]): string[] | null {
+    const codes = codeBreakouts.flatMap(code => !!code?.detailedCode() ? [code.detailedCode() as string] : [])
+    return this.dedupeCodes(codes)
   }
 
-  getDetailedJobCodes(): string[] {
-    return this.getJobCodes(JobCodeType.Detailed)
+  private inferJobRoleCodes(codeBreakouts: JobCodeBreakout[]): string[] | null {
+    const codes = codeBreakouts.flatMap(code => !!code?.jobRoleCode() ? [code.jobRoleCode() as string] : [])
+    return this.dedupeCodes(codes)
   }
 
-  getJobRoleCodes(): string[] {
-    return this.getJobCodes(JobCodeType.JobRole)
+  private dedupeCodes(codes: string[]): string[] | null {
+    const withoutDupes = [...new Set(codes)]
+    return withoutDupes.length > 0 ? withoutDupes : null
   }
 }
 

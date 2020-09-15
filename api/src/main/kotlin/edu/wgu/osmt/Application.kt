3da37d9d0554fa23f09454e664edc5a210e7cc43
Josh Kapple <jkapple@concentricsky.com>
2020-09-14 23:39:14 -0700

    OSMT-402 Health Check - add JPA default connection for Mysql healthcheck - setup Spring-Boot Starter Actuator for health check at /actuator/health - Add `review` application properties file - add git.properties to build process for /actuator/info

--- api/src/main/kotlin/edu/wgu/osmt/Application.kt
+++ api/src/main/kotlin/edu/wgu/osmt/Application.kt
@@ -4,12 +4,11 @@ import edu.wgu.osmt.db.DbConfig
 import edu.wgu.osmt.elasticsearch.EsConfig
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.autoconfigure.flyway.FlywayAutoConfiguration
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.context.properties.EnableConfigurationProperties
 import org.springframework.boot.runApplication
 
-@SpringBootApplication(exclude = arrayOf(DataSourceAutoConfiguration::class, FlywayAutoConfiguration::class))
+@SpringBootApplication(exclude = arrayOf(FlywayAutoConfiguration::class))
 @ConfigurationPropertiesScan("edu.wgu.osmt.config")
 @EnableConfigurationProperties(DbConfig::class, EsConfig::class)
 class Application

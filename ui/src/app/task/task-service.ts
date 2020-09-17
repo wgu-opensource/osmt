ed4ac8ee013b651a85976d3042afe091051a3fad
jcleary <jcleary@concentricsky.com>
2020-09-17 12:03:33 -0700

    [OSMT-407] Abstracted get into generic function that always perform baseURL substitution

--- ui/src/app/task/task-service.ts
+++ ui/src/app/task/task-service.ts
@@ -1,29 +1,33 @@
 import { Injectable } from "@angular/core"
-import { HttpClient } from "@angular/common/http"
+import {HttpClient, HttpResponse} from "@angular/common/http"
 import { Observable } from "rxjs"
 import { ITaskResponse, TaskInProgress } from "./TaskInProgress"
 import { map } from "rxjs/operators"
+import {AbstractService} from "../abstract.service"
 
 @Injectable({
   providedIn: "root"
 })
-export class TaskService {
-  constructor(private httpClient: HttpClient) {
+export class TaskService extends AbstractService {
+
+  constructor(httpClient: HttpClient) {
+    super(httpClient)
   }
 
   private serviceUrl = "api/tasks/"
 
 
   startAllSkillsCsvTask(): Observable<ITaskResponse> {
-    return this.httpClient
-      .get<ITaskResponse>(`api/skills`, {
-        headers: {
-          Accept: "text/csv"
-        }
-      })
-      .pipe(map((xs: ITaskResponse) => new TaskInProgress(xs)))
+    const failureMessage = "Could not start a skills csv export"
+    return this.get<ITaskResponse>({
+      path: "api/skills",
+      headers: {
+        Accept: "text/csv"
+      }
+    }).pipe(map(({body}) => new TaskInProgress(this.safeUnwrapBody(body, failureMessage))))
   }
 
+  // this call is a bit different since it's returning a csv for immediate download, so use httpClient's get() method
   // tslint:disable-next-line:no-any
   getTaskResultsIfComplete(uuid: string): Observable<any> {
     return this.httpClient

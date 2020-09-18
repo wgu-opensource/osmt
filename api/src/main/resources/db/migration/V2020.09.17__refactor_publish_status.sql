8bc16286b14a71af8e5af1fc61f15de66b625309
Josh Kapple <jkapple@concentricsky.com>
2020-09-18 11:37:56 -0700

    OSMT-377 - drop PublishStatus table

--- api/src/main/resources/db/migration/V2020.09.17__refactor_publish_status.sql
+++ api/src/main/resources/db/migration/V2020.09.17__refactor_publish_status.sql
@@ -5,6 +5,8 @@ DROP FOREIGN KEY `fk_RichSkillDescriptor_publish_status_id_id`,
 ADD COLUMN `archiveDate` datetime(6) null,
 ADD COLUMN `publishDate` datetime(6) null;
 
+DROP TABLE IF EXISTS `PublishStatus`;
+
 ALTER TABLE `RichSkillDescriptor` DROP COLUMN publish_status_id;
 
 ALTER TABLE `Collection`

b16ae2c0928a567067a4d0588f89b3733a477f8c
Josh Kapple <jkapple@concentricsky.com>
2020-09-17 21:35:05 -0700

    OSMT-377 - Remove PublishStatus lookup table - Derive PublishStatus from new columns archiveDate and publishDate - Cleanup - only run rqueue on apiserver profile

---
+++ api/src/main/resources/db/migration/V2020.09.17__refactor_publish_status.sql
@@ -0,0 +1,14 @@
+USE osmt_db;
+
+ALTER TABLE `RichSkillDescriptor`
+DROP FOREIGN KEY `fk_RichSkillDescriptor_publish_status_id_id`,
+ADD COLUMN `archiveDate` datetime(6) null,
+ADD COLUMN `publishDate` datetime(6) null;
+
+ALTER TABLE `RichSkillDescriptor` DROP COLUMN publish_status_id;
+
+ALTER TABLE `Collection`
+ADD COLUMN `archiveDate` datetime(6) null,
+ADD COLUMN `publishDate` datetime(6) null;
+
+

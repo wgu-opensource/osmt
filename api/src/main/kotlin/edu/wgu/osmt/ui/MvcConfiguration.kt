6d0d4290a0048096f8215123bc40ed83f4fa243c
jcleary <jcleary@concentricsky.com>
2020-10-06 15:17:48 -0700

    [OSMT-436] Got jar assets serving with a catch...

--- api/src/main/kotlin/edu/wgu/osmt/ui/MvcConfiguration.kt
+++ api/src/main/kotlin/edu/wgu/osmt/ui/MvcConfiguration.kt
@@ -2,10 +2,7 @@ package edu.wgu.osmt.ui
 
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.CacheControl
-import org.springframework.web.servlet.config.annotation.CorsRegistry
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry
-import org.springframework.web.servlet.config.annotation.ViewControllerRegistry
-import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
+import org.springframework.web.servlet.config.annotation.*
 import java.util.concurrent.TimeUnit
 
 
@@ -13,14 +10,16 @@ import java.util.concurrent.TimeUnit
 class UiAppConfig : WebMvcConfigurer {
     override fun addResourceHandlers(registry: ResourceHandlerRegistry) {
         registry.addResourceHandler("/**")
-            .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
-            .addResourceLocations("classpath:/ui/")
+                .setCacheControl(CacheControl.maxAge(365, TimeUnit.DAYS))
+                .addResourceLocations("classpath:/ui/", "classpath:/ui/images/")
     }
 
-    @Override
-    override fun addViewControllers(registry: ViewControllerRegistry) {
-        registry.addViewController("/{notApi:(?!api)[^\\.]*(?!\\.\\w+)}/**").setViewName("forward:/")
-    }
+    // TODO (julian) Temporarily had to be removed to stop redirecting away from assets
+    // The right move here seems to be to set this up to redirect against all non-asset paths only
+//    @Override
+//    override fun addViewControllers(registry: ViewControllerRegistry) {
+//        registry.addViewController("/{notApi:(?!api)[^\\.]*(?!\\.\\w+)}/**").setViewName("forward:/")
+//    }
 
     override fun addCorsMappings(registry: CorsRegistry) {
         registry.addMapping("/**")

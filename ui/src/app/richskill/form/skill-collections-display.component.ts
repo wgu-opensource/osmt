5d9408a0bc03893513e174e52bb85069041d6067
Wiggins <wiggins@concentricsky.com>
2020-10-08 21:03:27 -0700

    OSMT-315: collections on skill edit lets you undo and saves on form save

--- ui/src/app/richskill/form/skill-collections-display.component.ts
+++ ui/src/app/richskill/form/skill-collections-display.component.ts
@@ -15,21 +15,42 @@ export class SkillCollectionsDisplayComponent implements OnInit {
   @Input() required: boolean = false
   @Input() name: string = ""
 
+  toRemove: string[] = []
+  allCollections: string[] = []
+
   constructor() {
   }
 
   ngOnInit(): void {
+    this.allCollections = this.control.value.slice()
+  }
+
+  handleClickUndo(collection: string): boolean {
+    const idx = this.toRemove.indexOf(collection)
+    if (idx !== -1) {
+      this.toRemove.splice(idx, 1)
+      this.control.setValue(this.control.value.concat(collection))
+
+      if (this.toRemove.length < 1) {
+        this.control.markAsPristine()
+        this.control.markAsUntouched()
+      }
+    }
+
+    return false
   }
 
   handleClickRemove(collection: string): boolean {
     const copy: string[] = this.control.value
     const idx = copy.indexOf(collection)
+    this.toRemove.push(collection)
     if (idx !== -1) {
       copy.splice(idx, 1) // remove element from copy
       this.control.markAsDirty()
       this.control.markAsTouched()
       this.control.setValue(copy)
     }
+
     return false
   }
 }

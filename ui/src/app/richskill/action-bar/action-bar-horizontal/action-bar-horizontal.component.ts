b79ede93568518be5f1e08270db8875032eebb52
jcleary <jcleary@concentricsky.com>
2020-10-09 14:04:38 -0700

    [OSMT-507] Copy URL button copies to clipboard

--- ui/src/app/richskill/action-bar/action-bar-horizontal/action-bar-horizontal.component.ts
+++ ui/src/app/richskill/action-bar/action-bar-horizontal/action-bar-horizontal.component.ts
@@ -1,17 +1,27 @@
 import { Component, OnInit } from "@angular/core"
+import {Router} from "@angular/router"
+import {AppConfig} from "../../../app.config"
 
 @Component({
   selector: "app-action-bar-horizontal",
   templateUrl: "./action-bar-horizontal.component.html"
 })
 export class ActionBarHorizontalComponent implements OnInit {
-  constructor() { }
+
+  constructor(private router: Router) { }
+
+  // This gets mapped to a visually hidden textarea so that javascript has a node to copy from
+  href = ""
 
   ngOnInit(): void {
+    this.href = `${AppConfig.settings.baseApiUrl}${this.router.url}`
   }
 
-  onCopyURL(): void {
-    console.log("You just clicked Copy URL")
+  onCopyURL(fullPath: HTMLTextAreaElement): void {
+    console.log(`copying ${fullPath.textContent}`)
+    fullPath.select()
+    document.execCommand("copy")
+    fullPath.setSelectionRange(0, 0)
   }
 
   onDownloadCsv(): void {
--- ui/src/app/richskill/action-bar/action-bar.component.ts
+++ ui/src/app/richskill/action-bar/action-bar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from "@angular/core"
+import { Router } from "@angular/router"
+import {AppConfig} from "../../app.config"
 
 @Component({
   selector: "app-action-bar",
@@ -6,13 +8,20 @@ import { Component, OnInit } from "@angular/core"
 })
 export class ActionBarComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  // This gets mapped to a visually hidden textarea so that javascript has a node to copy from
+  href = ""
 
   ngOnInit(): void {
+    this.href = `${AppConfig.settings.baseApiUrl}${this.router.url}`
   }
 
-  onCopyURL(): void {
-    console.log("You just clicked Copy URL")
+  onCopyURL(fullPath: HTMLTextAreaElement): void {
+    console.log(`copying ${fullPath.textContent}`)
+    fullPath.select()
+    document.execCommand("copy")
+    fullPath.setSelectionRange(0, 0)
   }
 
   onDownloadCsv(): void {

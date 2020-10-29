612b858e05a6fdc2baec1df08c62c28e60c91233
Francisco Gray <fgray@concentricsky.com>
2020-10-29 14:41:47 -0700

    Specifies CHARSET and COLLATION for every table create
    (cherry picked from commit 741447e07ff168a140526140d22da54053757a2f)

--- api/src/main/resources/db/migration/V2020.09.15__initial_schema.sql
+++ api/src/main/resources/db/migration/V2020.09.15__initial_schema.sql
@@ -31,8 +31,7 @@ CREATE TABLE IF NOT EXISTS `AuditLog`
     `entityId`      bigint(20)   NOT NULL,
     `changedFields` text         NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 5;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci AUTO_INCREMENT = 5;
 
 --
 -- Table structure for table `JobCode`
@@ -54,7 +53,7 @@ CREATE TABLE IF NOT EXISTS `JobCode`
     `url`          varchar(1024) DEFAULT NULL,
     PRIMARY KEY (`id`),
     KEY `idx_JobCode_code` (`code`)
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Table structure for table `Keyword`
@@ -72,15 +71,15 @@ CREATE TABLE IF NOT EXISTS `Keyword`
     KEY `idx_Keyword_uri` (`keyword_type_enum`, `uri`),
 
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
+
 
 CREATE TABLE IF NOT EXISTS `PublishStatus`
 (
     `id`   bigint(20)  NOT NULL,
     `name` varchar(64) NOT NULL,
     PRIMARY KEY (`id`)
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 1;
+) ENGINE = InnoDB AUTO_INCREMENT = 1 CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Dumping data for table `PublishStatus`
@@ -120,8 +119,7 @@ CREATE TABLE IF NOT EXISTS `RichSkillDescriptor`
     CONSTRAINT `fk_RichSkillDescriptor_cat_id_id` FOREIGN KEY (`cat_id`) REFERENCES `Keyword` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT `fk_RichSkillDescriptor_author_id_id` FOREIGN KEY (`author_id`) REFERENCES `Keyword` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE,
     CONSTRAINT `fk_RichSkillDescriptor_publish_status_id_id` FOREIGN KEY (`publish_status_id`) REFERENCES `PublishStatus` (`id`) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE = InnoDB
-  AUTO_INCREMENT = 3;
+) ENGINE = InnoDB AUTO_INCREMENT = 3 CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 
 --
@@ -137,7 +135,7 @@ CREATE TABLE IF NOT EXISTS `RichSkillJobCodes`
     KEY `fk_RichSkillJobCodes_jobcode_id_id` (`jobcode_id`),
     CONSTRAINT `fk_RichSkillJobCodes_jobcode_id_id` FOREIGN KEY (`jobcode_id`) REFERENCES `JobCode` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `fk_RichSkillJobCodes_richskill_id_id` FOREIGN KEY (`richskill_id`) REFERENCES `RichSkillDescriptor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 --
 -- Table structure for table `RichSkillKeywords`
@@ -152,7 +150,7 @@ CREATE TABLE IF NOT EXISTS `RichSkillKeywords`
     KEY `fk_RichSkillKeywords_keyword_id_id` (`keyword_id`),
     CONSTRAINT `fk_RichSkillKeywords_keyword_id_id` FOREIGN KEY (`keyword_id`) REFERENCES `Keyword` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT `fk_RichSkillKeywords_richskill_id_id` FOREIGN KEY (`richskill_id`) REFERENCES `RichSkillDescriptor` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS `Collection`
 (
@@ -164,7 +162,7 @@ CREATE TABLE IF NOT EXISTS `Collection`
     `author_id`    BIGINT      NULL,
     CONSTRAINT fk_Collection_author_id_id
         FOREIGN KEY (author_id) REFERENCES Keyword (id) ON DELETE RESTRICT ON UPDATE CASCADE
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 CREATE TABLE IF NOT EXISTS CollectionSkills
 (
@@ -173,9 +171,10 @@ CREATE TABLE IF NOT EXISTS CollectionSkills
     CONSTRAINT PK_CollectionSkills_c_rs PRIMARY KEY (collection_id, skill_id),
     CONSTRAINT fk_CollectionSkills_collection_id_id FOREIGN KEY (collection_id) REFERENCES Collection (id) ON DELETE CASCADE ON UPDATE CASCADE,
     CONSTRAINT fk_CollectionSkills_skill_id_id FOREIGN KEY (skill_id) REFERENCES RichSkillDescriptor (id) ON DELETE CASCADE ON UPDATE CASCADE
-) ENGINE = InnoDB;
+) ENGINE = InnoDB CHARSET=utf8mb3 COLLATE=utf8mb3_unicode_ci;
 
 CALL createIndexIfNotExist('CollectionSkills', 'CollectionSkills_collection_id', 'collection_id');
 CALL createIndexIfNotExist('CollectionSkills', 'CollectionSkills_skill_id', 'skill_id');
 #CREATE INDEX CollectionSkills_collection_id ON CollectionSkills (collection_id);
 #CREATE INDEX CollectionSkills_skill_id ON CollectionSkills (skill_id);
+

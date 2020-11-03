6a89ece0050da01d8f4008d3e11a32054261e144
Wiggins <wiggins@concentricsky.com>
2020-11-03 08:33:19 -0800

    detail-card-status-bar on table row

--- ui/src/app/detail-card/status-bar/detail-card-status-bar.component.ts
+++ ui/src/app/detail-card/status-bar/detail-card-status-bar.component.ts
@@ -21,7 +21,7 @@ export class DetailCardStatusBarComponent implements OnInit {
   }
 
   isPublished(): boolean {
-    return this.status === PublishStatus.Published
+    return this.status !== PublishStatus.Unpublished
   }
 
   isArchived(): boolean {

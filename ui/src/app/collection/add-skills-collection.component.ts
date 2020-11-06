c6ab2249b0e89b206ef57590bcb8b8368d80ea8b
Wiggins <wiggins@concentricsky.com>
2020-11-06 02:31:53 -0800

    OSMT-445: rough out add skills to collection

--- ui/src/app/collection/add-skills-collection.component.ts
+++ ui/src/app/collection/add-skills-collection.component.ts
@@ -3,21 +3,35 @@ import {Router} from "@angular/router";
 import {ApiSkillSummary} from "../richskill/ApiSkillSummary";
 import {Title} from "@angular/platform-browser";
 import {PublishStatus} from "../PublishStatus";
+import {FormControl, FormGroup} from "@angular/forms";
+import {Observable} from "rxjs";
+import {ApiSearch, PaginatedCollections, PaginatedSkills} from "../richskill/service/rich-skill-search.service";
+import {ApiSkillSortOrder} from "../richskill/ApiSkill";
+import {CollectionService} from "./service/collection.service";
 
 @Component({
   selector: "app-add-skills-collection",
   templateUrl: "./add-skills-collection.component.html"
 })
 export class AddSkillsCollectionComponent implements OnInit {
-  selectedSkills?: ApiSkillSummary[]
+  from = 0
+  size = 50
 
-  selectedFilters: Set<PublishStatus> = new Set([PublishStatus.Unpublished, PublishStatus.Published])
+  resultsLoaded: Observable<PaginatedCollections> | undefined
+  results: PaginatedCollections | undefined
 
-  get isPlural(): boolean {
-    return (this.selectedSkills?.length ?? 0) > 1
+  searchForm = new FormGroup({
+    search: new FormControl("")
+  })
+  public get searchQuery(): string {
+    return this.searchForm.get("search")?.value ?? ""
   }
 
-  constructor(protected router: Router, protected titleService: Title) {
+  columnSort: ApiSkillSortOrder = ApiSkillSortOrder.CategoryAsc
+  selectedSkills?: ApiSkillSummary[]
+  selectedFilters: Set<PublishStatus> = new Set([PublishStatus.Unpublished, PublishStatus.Published])
+
+  constructor(protected router: Router, protected titleService: Title, protected collectionService: CollectionService) {
     this.selectedSkills = this.router.getCurrentNavigation()?.extras.state as ApiSkillSummary[]
     this.titleService.setTitle("Add RSDs to a Collection")
   }
@@ -27,8 +41,81 @@ export class AddSkillsCollectionComponent implements OnInit {
       this.router.navigate(["/"])
     }
   }
+  handleDefaultSubmit(): boolean {
+    this.loadNextPage()
+    this.from = 0
+
+    return false
+  }
+
+  loadNextPage(): void {
+    const query = this.searchQuery.trim()
+
+    if (this.selectedFilters.size < 1 || query.length < 1) {
+      this.setResults(new PaginatedCollections([], 0))
+      return
+    }
+
+    const apiSearch = ApiSearch.factory({query})
+    this.resultsLoaded = this.collectionService.searchCollections(apiSearch, this.size, this.from, this.selectedFilters, this.columnSort)
+    this.resultsLoaded.subscribe((results) => {
+      this.setResults(results)
+    })
+  }
+
+  get isPlural(): boolean {
+    return (this.selectedSkills?.length ?? 0) > 1
+  }
+
+  clearSearch(): boolean {
+    this.searchForm.reset()
+    return false
+  }
 
   handleFiltersChanged(newFilters: Set<PublishStatus>): void {
     this.selectedFilters = newFilters
+    this.loadNextPage()
+  }
+
+  handlePageClicked(newPageNo: number): void {
+    this.from = (newPageNo - 1) * this.size
+    this.loadNextPage()
+  }
+
+  handleHeaderColumnSort(sort: ApiSkillSortOrder): void {
+    this.columnSort = sort
+    this.from = 0
+    this.loadNextPage()
+  }
+
+  protected setResults(results: PaginatedCollections): void {
+    this.results = results
+    this.selectedSkills = undefined
+  }
+
+  get totalCount(): number {
+    return this.results?.totalCount ?? 0
+  }
+
+  get curPageCount(): number {
+    return this.results?.collections.length ?? 0
+  }
+
+  get emptyResults(): boolean {
+    return this.curPageCount < 1
+  }
+
+  get firstRecordNo(): number {
+    return this.from + 1
+  }
+  get lastRecordNo(): number {
+    return Math.min(this.from + this.curPageCount, this.totalCount)
+  }
+
+  get totalPageCount(): number {
+    return Math.ceil(this.totalCount / this.size)
+  }
+  get currentPageNo(): number {
+    return Math.floor(this.from / this.size) + 1
   }
 }

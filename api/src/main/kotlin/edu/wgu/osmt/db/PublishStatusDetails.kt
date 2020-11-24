1bffdf83fc079a0850c70a8b451e80165d2db805
Wiggins <wiggins@concentricsky.com>
2020-11-24 05:31:11 -0800

    OSMT-965: archiveDate doesnt need to be after publishDate

--- api/src/main/kotlin/edu/wgu/osmt/db/PublishStatusDetails.kt
+++ api/src/main/kotlin/edu/wgu/osmt/db/PublishStatusDetails.kt
@@ -1,9 +1,7 @@
 package edu.wgu.osmt.db
 
 import org.jetbrains.exposed.sql.Column
-import org.jetbrains.exposed.sql.statements.UpdateBuilder
 import java.time.LocalDateTime
-import java.time.ZoneOffset
 
 interface PublishStatusUpdate<UpdateObjectType> where UpdateObjectType : UpdateObject<*>, UpdateObjectType: HasPublishStatus {
     val archiveDate: Column<LocalDateTime?>
@@ -17,7 +15,7 @@ interface PublishStatusDetails{
     fun publishStatus(): PublishStatus{
         return when {
             archiveDate != null && publishDate == null -> PublishStatus.Deleted
-            archiveDate != null && archiveDate!!.isAfter(publishDate) -> PublishStatus.Archived
+            archiveDate != null && publishDate != null -> PublishStatus.Archived
             publishDate != null -> PublishStatus.Published
             else -> PublishStatus.Draft
         }

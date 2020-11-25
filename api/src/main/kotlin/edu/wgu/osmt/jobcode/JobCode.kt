6664e943e1af0e7b753abb9909fad1e4822cd32b
Wiggins <wiggins@concentricsky.com>
2020-11-25 04:26:09 -0800

    Revert "Revert "Feature/osmt 851 search expectations""
    This reverts commit 4bbb65e2aeaffd1b7456c8dc855d46c1c0f77c4d.

--- api/src/main/kotlin/edu/wgu/osmt/jobcode/JobCode.kt
+++ api/src/main/kotlin/edu/wgu/osmt/jobcode/JobCode.kt
@@ -1,12 +1,10 @@
 package edu.wgu.osmt.jobcode
 
 import com.fasterxml.jackson.annotation.JsonIgnore
+import edu.wgu.osmt.csv.HasCodeHierarchy
 import edu.wgu.osmt.db.DatabaseData
 import org.elasticsearch.common.Nullable
-import org.springframework.data.elasticsearch.annotations.DateFormat
-import org.springframework.data.elasticsearch.annotations.Document
-import org.springframework.data.elasticsearch.annotations.Field
-import org.springframework.data.elasticsearch.annotations.FieldType
+import org.springframework.data.elasticsearch.annotations.*
 import java.time.LocalDateTime
 import java.time.ZoneOffset
 
@@ -21,33 +19,64 @@ data class JobCode(
     @Field(type = FieldType.Date, format = DateFormat.basic_date_time)
     override val creationDate: LocalDateTime,
 
-    @Field(type = FieldType.Keyword)
+    @MultiField(
+        mainField = Field(type = FieldType.Search_As_You_Type),
+        otherFields = [InnerField(suffix = "keyword", type = FieldType.Keyword)]
+    )
     val major: String? = null,             // bls major category name
 
-    @Field(type = FieldType.Keyword)
+    @MultiField(
+        mainField = Field(type = FieldType.Search_As_You_Type),
+        otherFields = [InnerField(suffix = "keyword", type = FieldType.Keyword)]
+    )
     val minor: String? = null,             // bls minor category name
 
-    @Field(type = FieldType.Keyword)
+    @MultiField(
+        mainField = Field(type = FieldType.Search_As_You_Type),
+        otherFields = [InnerField(suffix = "keyword", type = FieldType.Keyword)]
+    )
     val broad: String? = null,             // bls broad category name
 
-    @Field(type = FieldType.Keyword)
+    @MultiField(
+        mainField = Field(type = FieldType.Search_As_You_Type),
+        otherFields = [InnerField(suffix = "keyword", type = FieldType.Keyword)]
+    )
     val detailed: String? = null,          // bls detailed (for o*net level codes -- blank for bls detailed)
 
-    @Field(type = FieldType.Keyword)
+    @MultiField(
+        mainField = Field(type = FieldType.Search_As_You_Type),
+        otherFields = [InnerField(suffix = "keyword", type = FieldType.Keyword)]
+    )
     val code: String,                           // bls detailed code or a o*net code: XX-XXXX or XX-XXXX.XX
 
-    @Field
+    @Field(type = FieldType.Search_As_You_Type)
     val name: String? = null,                   // human readable label
 
-    @Field
+    @Field(type = FieldType.Search_As_You_Type)
     val description: String? = null,
 
-    @Field
+    @Field(type = FieldType.Search_As_You_Type)
     val framework: String? = null,               // e.g.: "bls" or "o*net"
 
     @Field
     val url: String? = null                     // e.g.: "http://onetonline/an/example/of/a/jobcode/canonicalUri"
 ) : DatabaseData {
+
+    @Field
+    @Nullable
+    val majorCode: String? = JobCodeBreakout.majorCode(code)
+
+    @Field
+    @Nullable
+    val minorCode: String? = JobCodeBreakout.minorCode(code)
+
+    @Field
+    @Nullable
+    val broadCode: String? = JobCodeBreakout.broadCode(code)
+
+    @Field
+    @Nullable
+    val jobRoleCode: String? = JobCodeBreakout.jobRoleCode(code)
 
     companion object {
         fun create(code: String): JobCode {

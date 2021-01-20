f7188d938a376125099549bd06821cd513396bf5
Wiggins <wiggins@concentricsky.com>
2021-01-20 08:23:18 -0800

    OSMT-736: page title - collection library

--- ui/src/app/table/collections-library.component.ts
+++ ui/src/app/table/collections-library.component.ts
@@ -5,6 +5,7 @@ import {PaginatedCollections} from "../richskill/service/rich-skill-search.servi
 import {CollectionService} from "../collection/service/collection.service";
 import {CollectionsListComponent} from "../collection/collections-list.component";
 import {determineFilters} from "../PublishStatus";
+import {Title} from "@angular/platform-browser";
 
 @Component({
   selector: "app-collections-library",
@@ -17,11 +18,13 @@ export class CollectionsLibraryComponent extends CollectionsListComponent implem
     protected router: Router,
     protected toastService: ToastService,
     protected collectionService: CollectionService,
+    protected titleService: Title
   ) {
     super(router, toastService, collectionService)
   }
 
   ngOnInit(): void {
+    this.titleService.setTitle(`Collections | ${this.whitelabel.toolName}`)
     this.loadNextPage()
   }
 

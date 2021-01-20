b5c1123fa430908cdb175506e472c3d29077232e
Wiggins <wiggins@concentricsky.com>
2021-01-20 08:29:15 -0800

    OSMT-736: page title - server errors

--- ui/src/app/loading/server-error.component.ts
+++ ui/src/app/loading/server-error.component.ts
@@ -1,11 +1,19 @@
-import {Component, Input} from '@angular/core';
+import {Component, Input} from "@angular/core"
+import {Whitelabelled} from "../../whitelabel"
+import {Title} from "@angular/platform-browser"
 
 
 @Component({
   selector: "app-server-error",
   templateUrl: "./server-error.component.html"
 })
-export class ServerErrorComponent {
+export class ServerErrorComponent extends Whitelabelled {
   @Input() className = ""
   @Input() status: number = 500
+
+  constructor(protected titleService: Title) {
+    super()
+    this.titleService.setTitle(`Error | ${this.whitelabel.toolName}`)
+  }
+
 }

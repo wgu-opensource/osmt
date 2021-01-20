356dda7115cdbb5f6f11dca23553338fefee3a74
Wiggins <wiggins@concentricsky.com>
2021-01-20 07:57:57 -0800

    OSMT-736: page title - search results

--- ui/src/app/core/quick-links-helper.ts
+++ ui/src/app/core/quick-links-helper.ts
@@ -1,5 +1,6 @@
+import {Whitelabelled} from "../../whitelabel";
 
-export class QuickLinksHelper {
+export class QuickLinksHelper  extends Whitelabelled {
   focusAndScrollIntoView(target: HTMLElement, tagName?: string): boolean {
     const t = (tagName !== undefined) ? (target.getElementsByTagName(tagName)[0] as HTMLElement ?? target) : target
     if (t) {

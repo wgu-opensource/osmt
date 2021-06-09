562892677d9b6db21c71485b90df870c7014beb7
Wiggins <wiggins@concentricsky.com>
2021-06-09 06:00:23 -0700

    OSMT-1222: include all alignments in RichSkillCsvExport

--- api/src/main/kotlin/edu/wgu/osmt/richskill/RichSkillCsvExport.kt
+++ api/src/main/kotlin/edu/wgu/osmt/richskill/RichSkillCsvExport.kt
@@ -11,27 +11,33 @@ class RichSkillCsvExport(
 ): CsvResource<RichSkillAndCollections>("RichSkillCsvExport") {
     val listDelimeter = "; "
 
-    override fun columnTranslations(): Array<CsvColumn<RichSkillAndCollections>> {
-        return arrayOf(
-            return arrayOf(
-                CsvColumn("Canonical URL") { it.rs.canonicalUrl(appConfig.baseUrl) },
-                CsvColumn("RSD Name") { it.rs.name },
-                CsvColumn("Author") { it.rs.author?.value ?: "" },
-                CsvColumn("Skill Statement") { it.rs.statement },
-                CsvColumn("Category") { it.rs.category?.value ?: "" },
-                CsvColumn("Keywords") { it.rs.searchingKeywords.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
-                CsvColumn("Standards") { it.rs.standards.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
-                CsvColumn("Certifications") { it.rs.certifications.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
-                CsvColumn("Occupation Major Groups") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::majorCode) },
-                CsvColumn("Occupation Minor Groups") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::minorCode) },
-                CsvColumn("Broad Occupations") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::broadCode) },
-                CsvColumn("Detailed Occupations") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::detailedCode) },
-                CsvColumn("O*Net Job Codes") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::jobRoleCode) },
-                CsvColumn("Employers") { it.rs.employers.map {keyword -> keyword.value ?: ""}.joinToString(listDelimeter) },
-                CsvColumn("Alignment Name") { it.rs.alignments.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
-                CsvColumn("Alignment URL") { it.rs.alignments.map { keyword -> keyword.uri ?: "" }.joinToString(listDelimeter) }
-            )
+    override fun columnTranslations(data: List<RichSkillAndCollections>): Array<CsvColumn<RichSkillAndCollections>> {
+        val columns = arrayOf(
+            CsvColumn<RichSkillAndCollections>("Canonical URL") { it.rs.canonicalUrl(appConfig.baseUrl) },
+            CsvColumn("RSD Name") { it.rs.name },
+            CsvColumn("Author") { it.rs.author?.value ?: "" },
+            CsvColumn("Skill Statement") { it.rs.statement },
+            CsvColumn("Category") { it.rs.category?.value ?: "" },
+            CsvColumn("Keywords") { it.rs.searchingKeywords.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
+            CsvColumn("Standards") { it.rs.standards.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
+            CsvColumn("Certifications") { it.rs.certifications.map { keyword -> keyword.value ?: "" }.joinToString(listDelimeter) },
+            CsvColumn("Occupation Major Groups") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::majorCode) },
+            CsvColumn("Occupation Minor Groups") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::minorCode) },
+            CsvColumn("Broad Occupations") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::broadCode) },
+            CsvColumn("Detailed Occupations") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::detailedCode) },
+            CsvColumn("O*Net Job Codes") { prepareJobCodePart(it.rs.jobCodes, JobCodeBreakout::jobRoleCode) },
+            CsvColumn("Employers") { it.rs.employers.map {keyword -> keyword.value ?: ""}.joinToString(listDelimeter) }
         )
+        val alignmentCount = data.map { s -> s.rs.alignments.size }.max() ?: 0
+        val alignmentColumns = (0 until alignmentCount).flatMap { i ->
+            val label = if (i > 0) " ${i+1}" else ""
+            listOf(
+                CsvColumn<RichSkillAndCollections>("Alignment${label} Name") { it.rs.alignments[i].value ?: "" },
+                CsvColumn<RichSkillAndCollections>("Alignment${label} URL") { it.rs.alignments[i].uri ?: "" },
+                CsvColumn<RichSkillAndCollections>("Alignment${label} Framework") { it.rs.alignments[i].framework ?: "" }
+            )
+        }
+        return columns + alignmentColumns
     }
 
     private fun prepareJobCodePart(

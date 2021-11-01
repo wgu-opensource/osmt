cdf15bf8b20ba6fbdb6e72b1ed240e4b44c2c93b
John Kallies <3021949+JohnKallies@users.noreply.github.com>
2021-11-01 13:40:10 -0400

    remove existing environment.prod.ts file
    - this file is now built by the UI build, and git ignored

--- ui/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment = {
-  production: true,
-  baseApiUrl: "",
-  loginUrl: "/oauth2/authorization/okta",
-  dynamicWhitelabel: true,
-  whiteLabelUrl: "/whitelabel/whitelabel-jwk.json"
-};

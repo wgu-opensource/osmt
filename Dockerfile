cd8749966991e18add35793a9b29691bb2f40f81
John Kallies <3021949+JohnKallies@users.noreply.github.com>
2021-12-09 19:40:01 -0500

    Extract base Docker images and push to DockerHub
    This change extracts the aspects of the Docker build that can be
    pre-built
    - osmt-base (CentOS/OpenJDK11)
    - osmt-build (osmt-base + Maven 3.8.4)
    
    This change also adds tooling to build those base images and push
    them to WGU's opensource DockerHub registry. At present, there is
    no CI/CD for this, so it will remain a manul process for OSMT
    maintaners within WGU.
    
    - The Maven build using local source code still copys the project
      into the Docker build context.

--- Dockerfile
+++ Dockerfile
@@ -1,63 +1,20 @@
-################
-## BASE IMAGE ##
-################
-FROM centos:centos8.3.2011 as osmt-base
-
-LABEL Maintainer="WGU / OSN"
-LABEL Version="1.0"
-
-ENV JAVA_HOME=/etc/alternatives/jre
-ENV USER=osmt
-ENV BASE_DIR=/opt/${USER}
-
-# Install EPEL / Useful packages /
-RUN /usr/bin/yum install -y epel-release \
-    && /usr/bin/yum update -y \
-    && /usr/bin/yum remove -y java-1.8.0-openjdk* \
-    && /usr/bin/yum install -y curl java-11-openjdk wget
-
-# Add in configuration files
-ADD ./docker/etc /etc
-
-# Set a DNS lookup TTL to 10 seconds
-RUN sed -i 's/#networkaddress.cache.ttl=-1/networkaddress.cache.ttl=10/' ${JAVA_HOME}/conf/security/java.security
-
-# Create user
-RUN /usr/sbin/useradd -r -d ${BASE_DIR} -s /bin/bash ${USER} -k /etc/skel -m -U \
-    && mkdir -p ${BASE_DIR}/bin ${BASE_DIR}/build ${BASE_DIR}/logs ${BASE_DIR}/etc \
-    && chown -R ${USER}:${USER} ${BASE_DIR}
-
 ###########################
-## BUILD / COMPILE IMAGE ##
+## TEMPORARY BUILD IMAGE ##
 ###########################
-FROM osmt-base as build
-
-ENV M2_VERSION=3.8.4
-ENV M2_HOME=/usr/local/maven
-ENV PATH=${M2_HOME}/bin:${PATH}
-
-# Download / Install Maven
-ADD https://dlcdn.apache.org/maven/maven-3/${M2_VERSION}/binaries/apache-maven-${M2_VERSION}-bin.tar.gz /usr/share/src/
-
-WORKDIR /usr/share/src
-
-RUN tar -xf apache-maven-${M2_VERSION}-bin.tar.gz \
-    && mv apache-maven-${M2_VERSION} ${M2_HOME}/
+FROM wguopensource/osmt-build:latest as build
 
 # Copy in source code.
-COPY --chown=${USER}:${USER} ./ ${BASE_DIR}/build/
-
-WORKDIR ${BASE_DIR}/build
-
 USER ${USER}
+COPY --chown=${USER}:${USER} ./ ${BASE_DIR}/build/
+WORKDIR ${BASE_DIR}/build
 
 # The dockerfile-build Maven profile excludes certain api integration tests that require access to the Docker service.
 RUN mvn clean install -P dockerfile-build
 
 ######################
-### PACKAGING IMAGE ##
+## SPRING APP IMAGE ##
 ######################
-FROM osmt-base
+FROM wguopensource/osmt-base:latest
 
 COPY --from=build --chown=${USER}:${USER} ${BASE_DIR}/build/api/target/osmt-*.jar ${BASE_DIR}/bin/osmt.jar
 
---
+++ docker/base-images/bin/push_base_docker_images.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+set -u
+
+declare IMAGE_VERSION
+declare DOCKERHUB_USER_ID
+declare DOCKERHUB_PASSWORD
+
+function prepare() {
+  local project_dir; project_dir="$(git rev-parse --show-toplevel)"
+  if [[ -z "${project_dir}" ]]; then
+    echo_err "Pushing the base Docker images should be run from the directory context of the osmt git repo. Exiting..."
+    exit 134
+  fi
+
+  local script_dir; script_dir="${project_dir}/docker/base-images"
+  if [[ ! -d "${script_dir}" ||  ! -r "${script_dir}" ]]; then
+    echo_err "Can not change directory to ${script_dir}. Exiting..."
+    exit 138
+  fi
+
+  echo_info "Changing directory to ${script_dir}."
+  cd "${script_dir}"
+
+  echo_info "Sourcing .env file"
+  source image_version.env
+  if [[ -z "${IMAGE_VERSION}" ]]; then
+    echo_err "Pushing the base Docker images requires a IMAGE_VERSION env var in the image_version.env file. Exiting..."
+    exit 135
+  fi
+  echo_info "IMAGE_VERSION is ${IMAGE_VERSION}"
+}
+
+function authenticate() {
+  local rc
+
+  if [[ -z "${DOCKERHUB_USER_ID}" || -z "${DOCKERHUB_PASSWORD}" ]]; then
+    echo_err "Pushing Docker images to DockerHub requires environment variables for DOCKERHUB_USER_ID and DOCKERHUB_PASSWORD. Exiting..."
+    exit 135
+  fi
+
+  echo_info "Authenticating to DockerHub"
+  docker login -u "${DOCKERHUB_USER_ID}" -p "${DOCKERHUB_PASSWORD}"
+
+  rc=$?
+  if [[ $rc -ne 0 ]]; then
+    echo_err "Error authenticating to DockerHub. Exiting..."
+    exit 135
+  fi
+}
+
+function push() {
+  local image_name; image_name="${1}"
+  if [[ -z "${image_name}" ]]; then
+    echo_err "push() requires an image_name argument. Exiting..."
+    exit 135
+  fi
+
+ push_image "wguopensource/${image_name}:${IMAGE_VERSION}"
+ push_image "wguopensource/${image_name}:latest"
+
+}
+
+function push_image() {
+  local rc
+  local tag; tag="${1}"
+  echo_info "Pushing image for ${tag} to DockerHub"
+
+  docker push "${tag}"
+  rc=$?
+  if [[ $rc -ne 0 ]]; then
+    echo_err "Error pushing ${tag} to DockerHub. Exiting..."
+    exit 135
+  else
+    echo_info "${tag} successfully pushed to DockerHub."
+  fi
+}
+
+function logout() {
+  docker logout
+}
+
+function echo_info() {
+  echo "INFO: $@"
+}
+
+function echo_err() {
+  echo "ERROR: $@" 1>&2;
+}
+
+function main() {
+  echo_info "Starting the build for OSMT base Docker images"
+  prepare
+  authenticate
+  push "osmt-base"
+  push "osmt-build"
+  logout
+}
+
+trap logout ERR
+
+main

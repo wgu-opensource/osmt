9b80d2c4c891b71c630318b2345f96d746b691ca
Devon Sumner <devon.sumner@wgu.edu>
2022-08-16 17:23:31 -0600

    Added missing file.

---
+++ ui/src/app/auth/iauth-service.ts
@@ -0,0 +1,37 @@
+export interface IAuthService {
+  // Initialize auth service synchronously.  Must be called in main thread and not via async mechanism.
+  init(): void
+
+  // Finish initial setup of auth service.  Can be done lazily.
+  setup(): void
+
+  // Start the authentication process.
+  start(returnPath: string): void
+
+  // Store the token once it is received.
+  storeToken(accessToken: string): void
+
+  // Save where to return to.
+  storeReturn(returnRoute: string): void
+
+  // Return to previously saved location, but do so asynchronously.
+  restoreReturnAsync(): void
+
+  // Return to previously saved location and clear the saved value.
+  popReturn(): string | null
+
+  // Initiate the logout sequence.
+  logout(): void
+
+  // Get the stored auth token.
+  currentAuthToken(): string | null
+
+  // Check if we have a token.
+  isAuthenticated(): boolean
+
+  // Indicate whether the backend is down.
+  setServerIsDown(isDown: boolean): void
+
+  // Get the user's roles.
+  getRole(): string
+}

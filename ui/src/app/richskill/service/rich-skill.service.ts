85aa9565c2ca94a4e4575b1412bd08375113d1d0
Edwin Santizo <edwin.santizo@wgu.edu>
2022-10-03 22:56:56 -0600

    fixed bug were it would make multiple xhr requests found that with multiple subscribers to an observable it would remit the execution chain aka send xhr requests. We were using share method but share we were using it incorrectly. After reading the docs, shareReplay method allows for the replaying of values that was returned from the observable without making a costly xhr request for any amount of subscribres.

--- ui/src/app/richskill/service/rich-skill.service.ts
+++ ui/src/app/richskill/service/rich-skill.service.ts
@@ -141,7 +141,6 @@ export class RichSkillService extends AbstractService {
       params,
       body: apiSearch,
     })
-      .pipe(share())
       .pipe(map(({body, headers}) => {
         const totalCount = Number(headers.get("X-Total-Count"))
         const skills = body?.map(skill => skill) || []

7f7bb9cebb463e6122885cc180f2f1f75520d83f
Edwin Santizo <edwin.santizo@wgu.edu>
2022-11-13 17:49:07 -0700

    removed code from common component to library export component

--- ui/src/app/navigation/commoncontrols.component.ts
+++ ui/src/app/navigation/commoncontrols.component.ts
@@ -1,12 +1,9 @@
-import {Component, OnInit, LOCALE_ID, Inject} from "@angular/core"
-import {formatDate} from "@angular/common"
+import {Component, OnInit} from "@angular/core"
 import {AbstractSearchComponent} from "./abstract-search.component"
 import {SearchService} from "../search/search.service"
-import {RichSkillService} from "../richskill/service/rich-skill.service"
 import {ActivatedRoute} from "@angular/router"
 import {SvgHelper, SvgIcon} from "../core/SvgHelper"
 import {AuthService} from "../auth/auth-service"
-import * as FileSaver from "file-saver"
 
 @Component({
   selector: "app-commoncontrols",
@@ -20,27 +17,10 @@ export class CommoncontrolsComponent extends AbstractSearchComponent implements 
   constructor(
     protected searchService: SearchService,
     protected route: ActivatedRoute,
-    protected authService: AuthService,
-    protected richSkillService: RichSkillService,
-    @Inject(LOCALE_ID) protected locale: string) {
+    protected authService: AuthService) {
     super(searchService, route, authService)
   }
 
   ngOnInit(): void {
-  }
-  /**
-  * Added the logic for export library here because this controllers template holds the button
-  * The other links on the template link out to other templates and controllers while the export
-  * button required us to have a click action and call a method from this controller. We could possibly
-  * move it to the abstract search component or a helper since there are other onDownloadCsv functions
-  * that are similar to this
-  **/
-  onDownloadCsv(): void {
-    this.richSkillService.libraryExport()
-      .subscribe((csv: string) => {
-        const blob = new Blob([csv], {type: "text/csv;charset=utf-8;"})
-        const date = formatDate(new Date(), "yyyy-MM-dd", this.locale)
-        FileSaver.saveAs(blob, `RSD Library - OSMT ${date}.csv`)
-      })
   }
 }
---
+++ ui/src/app/navigation/libraryexport.component.ts
@@ -0,0 +1,41 @@
+import {Component, OnInit, LOCALE_ID, Inject} from "@angular/core"
+import {formatDate} from "@angular/common"
+import {SearchService} from "../search/search.service"
+import {RichSkillService} from "../richskill/service/rich-skill.service"
+import {ActivatedRoute} from "@angular/router"
+import {AuthService} from "../auth/auth-service"
+import * as FileSaver from "file-saver"
+import {SvgHelper, SvgIcon} from "../core/SvgHelper"
+import {CommoncontrolsComponent} from "./commoncontrols.component"
+import {AbstractSearchComponent} from "./abstract-search.component"
+
+@Component({
+  selector: "app-libraryexport",
+  templateUrl: "./libraryexport.component.html"
+})
+export class LibraryExportComponent extends AbstractSearchComponent implements OnInit {
+
+  searchIcon = SvgHelper.path(SvgIcon.SEARCH)
+  dismissIcon = SvgHelper.path(SvgIcon.DISMISS)
+
+  constructor(
+    protected searchService: SearchService,
+    protected route: ActivatedRoute,
+    protected authService: AuthService,
+    protected richSkillService: RichSkillService,
+    @Inject(LOCALE_ID) protected locale: string) {
+    super(searchService, route, authService)
+  }
+
+  ngOnInit(): void {
+  }
+
+  onDownloadLibrary(): void {
+    this.richSkillService.libraryExport()
+      .subscribe((csv: string) => {
+        const blob = new Blob([csv], {type: "text/csv;charset=utf-8;"})
+        const date = formatDate(new Date(), "yyyy-MM-dd", this.locale)
+        FileSaver.saveAs(blob, `RSD Library - OSMT ${date}.csv`)
+      })
+  }
+}

ae57f17c05ff0b2f27ed6cafe97749983a0aa4c7
Edwin Santizo <edwin.santizo@wgu.edu>
2022-11-14 22:30:01 -0700

    Created test spec file for LibraryExport component it tests the onDownloadLibrary function and makes sure it's called correctly

--- ui/test/resource/mock-stubs.ts
+++ ui/test/resource/mock-stubs.ts
@@ -336,6 +336,11 @@ export class RichSkillServiceStub {
   ): Observable<PaginatedSkills> {
     return of(createMockPaginatedSkills())
   }
+
+  // noinspection JSUnusedGlobalSymbols,JSUnusedLocalSymbols
+  libraryExport(): Observable<string> {
+    return of(`x, y, z`)
+  }
 }
 
 export let KeywordSearchServiceData = {

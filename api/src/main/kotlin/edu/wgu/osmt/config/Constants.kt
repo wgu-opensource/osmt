5bb0413fee885276d5a616ba45228631c763c1a0
John Kallies <3021949+JohnKallies@users.noreply.github.com>
2022-11-29 10:51:54 -0500

    extract ES index names as constants

--- api/src/main/kotlin/edu/wgu/osmt/config/Constants.kt
+++ api/src/main/kotlin/edu/wgu/osmt/config/Constants.kt
@@ -1,3 +1,11 @@
 package edu.wgu.osmt.config
 
 const val UNAUTHENTICATED_USERNAME = "unauthenticated"
+
+
+
+// ElasticSearch Index Names
+const val INDEX_RICHSKILL_DOC = "richskill_v1"
+const val INDEX_COLLECTION_DOC = "collection_v1"
+const val INDEX_JOBCODE_DOC = "jobcode_v1"
+const val INDEX_KEYWORD_DOC = "keyword"

5b79fb1f5890e2986a94185f14be9f3cb6438acd
Jose Chavez <j.chavez@wgu.edu>
2022-12-16 10:04:35 -0600

    Setting new constants for ES sort order

--- api/src/main/kotlin/edu/wgu/osmt/config/Constants.kt
+++ api/src/main/kotlin/edu/wgu/osmt/config/Constants.kt
@@ -10,10 +10,13 @@ const val NAME_ASC = "skill.asc"
 const val NAME_DESC = "skill.desc"
 
 
-
 // ElasticSearch Index Names
 const val INDEX_RICHSKILL_DOC = "richskill_v1"
 const val INDEX_COLLECTION_DOC = "collection_v1"
 const val INDEX_JOBCODE_DOC = "jobcode_v1"
 const val INDEX_KEYWORD_DOC = "keyword"
+
+// ElasticSearch Sort Criteria
+const val NAME_SORT_INSENSITIVE = "name.sort_insensitive"
+const val CATEGORY_SORT_INSENSITIVE = "category.sort_insensitive"
 

4c512ddb084bd093edf573953f4e8d95e05c2bad
Jose Chavez <j.chavez@wgu.edu>
2023-01-04 11:44:50 -0600

    changing method to be post

--- api/src/main/kotlin/edu/wgu/osmt/richskill/RichSkillController.kt
+++ api/src/main/kotlin/edu/wgu/osmt/richskill/RichSkillController.kt
@@ -222,7 +222,7 @@ class RichSkillController @Autowired constructor(
     }
 
     @Transactional(readOnly = true)
-    @GetMapping(RoutePaths.SKILL_CSV_LIST, produces = [MediaType.APPLICATION_JSON_VALUE])
+    @PostMapping(RoutePaths.SKILL_CSV_LIST, produces = [MediaType.APPLICATION_JSON_VALUE])
     @ResponseBody
     fun exportCustomList(
         @RequestBody uuids: List<String>?,

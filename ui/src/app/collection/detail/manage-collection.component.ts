fc7d39c67636f83797ebfaa4549576093d625dd8
manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
2023-01-18 16:11:05 -0600

    Download draft collection
    - Download draft collection is only available for admin.
    
    - Download published collection is available for all roles.

--- ui/src/app/collection/detail/manage-collection.component.ts
+++ ui/src/app/collection/detail/manage-collection.component.ts
@@ -13,9 +13,9 @@ import {determineFilters, PublishStatus} from "../../PublishStatus"
 import {ApiSkillSummary} from "../../richskill/ApiSkillSummary"
 import {Observable, of, Subject, throwError} from "rxjs"
 import {TableActionBarComponent} from "../../table/skills-library-table/table-action-bar.component"
-import {Title} from "@angular/platform-browser";
-import {AuthService} from "../../auth/auth-service";
-import {ButtonAction} from "../../auth/auth-roles";
+import {Title} from "@angular/platform-browser"
+import {AuthService} from "../../auth/auth-service"
+import {ButtonAction} from "../../auth/auth-roles"
 import {formatDate} from "@angular/common"
 import * as FileSaver from "file-saver"
 import {ITaskResult} from "../../task/ApiTaskResult"
@@ -226,7 +226,10 @@ export class ManageCollectionComponent extends SkillsListComponent implements On
       )
     }
 
-    if ((this.collection?.status === PublishStatus.Draft || this.collection?.status === PublishStatus.Published) && this.authService.isEnabledByRoles(ButtonAction.LibraryExport)) {
+    const isPublished = this.collection?.status === PublishStatus.Published
+    const isDraftAndUserIsAdmin = this.collection?.status === PublishStatus.Draft
+      && this.authService.isEnabledByRoles(ButtonAction.ExportDraftCollection)
+    if (isPublished || isDraftAndUserIsAdmin) {
       actions.push(new TableActionDefinition({
         label: "Download CSV",
         icon: this.downloadIcon,

4310c893df238ca0119a5d539aecebe925300772
manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
2023-01-20 10:32:09 -0600

    Working on delete collection request
    - Add request for delete collection.
    
    - Add delete collection with result.

--- ui/src/app/collection/service/collection.service.ts
+++ ui/src/app/collection/service/collection.service.ts
@@ -188,6 +188,16 @@ export class CollectionService extends AbstractService {
     return this.pollForTaskResult<ApiBatchResult>(this.updateSkills(collectionUuid, skillListUpdate, filterByStatus), pollIntervalMs)
   }
 
+  deleteCollectionWithResult(uuid: string): Observable<ApiBatchResult> {
+    return this.pollForTaskResult<ApiBatchResult>(this.deleteCollection(uuid))
+  }
+
+  deleteCollection(uuid: string): Observable<ApiTaskResult> {
+    return this.httpClient.delete<ITaskResult>(`api/collections/${uuid}/updateSkills`)
+      .pipe(share())
+      .pipe(map((body) => new ApiTaskResult(this.safeUnwrapBody(body, "unwrap failure"))))
+  }
+
   publishCollectionsWithResult(
     apiSearch: ApiSearch,
     newStatus: PublishStatus = PublishStatus.Published,

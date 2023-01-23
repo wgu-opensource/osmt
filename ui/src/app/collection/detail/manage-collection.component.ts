933ef4dcc3d0ab69348598496e8562bc5ebffd71
manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
2023-01-23 13:01:32 -0600

    Refresh when navigate
    - Refresh when navigate to collections after delete.

--- ui/src/app/collection/detail/manage-collection.component.ts
+++ ui/src/app/collection/detail/manage-collection.component.ts
@@ -260,7 +260,9 @@ export class ManageCollectionComponent extends SkillsListComponent implements On
     this.toastService.loaderSubject.next(true)
     this.collectionService.deleteCollectionWithResult(this.collection?.uuid ?? "").subscribe(() => {
       this.toastService.loaderSubject.next(false)
-      this.router.navigate(["/collections"])
+      this.router.navigate(["/collections"]).then(() => {
+        window.location.reload()
+      })
     })
   }
 

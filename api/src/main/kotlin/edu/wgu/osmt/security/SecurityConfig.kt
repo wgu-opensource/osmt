df024f446a6813cf7a97391dd0331828572f8590
Jose Chavez <j.chavez@wgu.edu>
2023-02-07 16:11:27 -0600

    securing workspace endpoint to be accessible by admins and curators only

--- api/src/main/kotlin/edu/wgu/osmt/security/SecurityConfig.kt
+++ api/src/main/kotlin/edu/wgu/osmt/security/SecurityConfig.kt
@@ -26,6 +26,7 @@ import edu.wgu.osmt.RoutePaths.TASK_DETAIL_SKILLS
 import edu.wgu.osmt.RoutePaths.TASK_DETAIL_TEXT
 import edu.wgu.osmt.api.model.ApiError
 import edu.wgu.osmt.config.AppConfig
+import edu.wgu.osmt.db.WORKSPACE
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
@@ -130,6 +131,7 @@ class SecurityConfig : WebSecurityConfigurerAdapter() {
             .mvcMatchers(POST, COLLECTION_UPDATE).hasAnyAuthority(ADMIN, CURATOR)
             .mvcMatchers(POST, COLLECTION_SKILLS_UPDATE).hasAnyAuthority(ADMIN)
             .mvcMatchers(DELETE, COLLECTION_REMOVE).hasAnyAuthority(ADMIN)
+            .mvcMatchers(GET, WORKSPACE).hasAnyAuthority(ADMIN, CURATOR)
 
             .mvcMatchers("/api/**").hasAnyAuthority(ADMIN, CURATOR, VIEW, READ)
     }

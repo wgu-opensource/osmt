dba816eb6bcef28aa214787eaa747082c6563602
Edwin Santizo <edwin.santizo@wgu.edu>
2023-02-12 21:41:06 -0700

    added resource for our osmt to be built by terraform

--- deploy/terraform/variables.tf
+++ deploy/terraform/variables.tf
@@ -1,16 +1,22 @@
-variable "aws_access_key" {
+variable "AWS_ACCESS_KEY" {
   type = string
   description = "AWS access key"
   sensitive = true
 }
 
-variable "aws_secret_key" {
+variable "AWS_SECRET_KEY" {
   type = string
   description = "AWS secret key"
   sensitive = true
 }
 
-variable "aws_region" {
+variable "AWS_SESSION_TOKEN" {
+  type = string
+  description = "AWS Session Token"
+  sensitive = true
+}
+
+variable "AWS_REGION" {
   type = string
   description = "AWS region to use for resources"
   default = "us-west-2"

cc3d5ff6934de883ee0fb86f73d085deb4027e22
Jose Chavez <j.chavez@wgu.edu>
2023-03-01 15:28:52 -0600

    Adding logic to retrieve all keywords incidences of a particular type

--- api/src/main/kotlin/edu/wgu/osmt/keyword/KeywordEsRepo.kt
+++ api/src/main/kotlin/edu/wgu/osmt/keyword/KeywordEsRepo.kt
@@ -25,26 +25,38 @@ interface CustomKeywordRepository {
 class CustomKeywordRepositoryImpl @Autowired constructor(override val elasticSearchTemplate: ElasticsearchRestTemplate) :
     CustomKeywordRepository {
     override fun typeAheadSearch(query: String, type: KeywordTypeEnum): SearchHits<Keyword> {
-        val limitedPageable = OffsetPageable(0, 10, null)
+        val limitedPageable: OffsetPageable
         val bq = QueryBuilders.boolQuery()
+        val nsq: NativeSearchQueryBuilder
 
-        val nsq: NativeSearchQueryBuilder =
-            NativeSearchQueryBuilder().withPageable(limitedPageable).withQuery(bq).withSort(SortBuilders.scoreSort())
-
-        bq
-            .must(QueryBuilders.termQuery(Keyword::type.name, type.name))
-            .should(
-                QueryBuilders.matchBoolPrefixQuery(
-                    Keyword::value.name,
-                    query
+        if(query.isEmpty()){ //retrieve all
+            limitedPageable = OffsetPageable(0, 10000, null)
+            nsq = NativeSearchQueryBuilder().withPageable(limitedPageable).withQuery(bq).withSort(SortBuilders.scoreSort())
+            bq
+                .must(QueryBuilders.termQuery(Keyword::type.name, type.name))
+                .should(
+                    QueryBuilders.matchAllQuery()
                 )
-            )
-            .should(
-                QueryBuilders.matchPhraseQuery(
-                    Keyword::value.name,
-                    query
-                ).boost(5f)
-            ).minimumShouldMatch(1)
+
+        }
+        else {
+            limitedPageable  = OffsetPageable(0, 10, null)
+            nsq = NativeSearchQueryBuilder().withPageable(limitedPageable).withQuery(bq).withSort(SortBuilders.scoreSort())
+            bq
+                .must(QueryBuilders.termQuery(Keyword::type.name, type.name))
+                .should(
+                    QueryBuilders.matchBoolPrefixQuery(
+                        Keyword::value.name,
+                        query
+                    )
+                )
+                .should(
+                    QueryBuilders.matchPhraseQuery(
+                        Keyword::value.name,
+                        query
+                    ).boost(5f)
+                ).minimumShouldMatch(1)
+        }
 
         return elasticSearchTemplate.search(nsq.build(), Keyword::class.java)
     }

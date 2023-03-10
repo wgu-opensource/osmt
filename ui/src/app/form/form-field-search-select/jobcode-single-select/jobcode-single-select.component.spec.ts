f20a04f788c61fe607abf5f56b92f245b3bb8900
manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
2023-03-10 11:43:02 -0600

    Add test form jobcode single select

--- ui/src/app/form/form-field-search-select/jobcode-single-select/jobcode-single-select.component.spec.ts
+++ ui/src/app/form/form-field-search-select/jobcode-single-select/jobcode-single-select.component.spec.ts
@@ -1,6 +1,12 @@
 import { ComponentFixture, TestBed } from "@angular/core/testing"
 
 import { JobcodeSingleSelectComponent } from "./jobcode-single-select.component"
+import {HttpClientTestingModule} from "@angular/common/http/testing"
+import {AuthService} from "../../../auth/auth-service"
+import {AuthServiceStub, KeywordSearchServiceStub} from "../../../../../test/resource/mock-stubs"
+import {AppConfig} from "../../../app.config"
+import {FormModule} from "../../form.module"
+import {KeywordSearchService} from "../../../richskill/service/keyword-search.service"
 
 describe("JobcodeSingleSelectComponent", () => {
   let component: JobcodeSingleSelectComponent
@@ -8,9 +14,21 @@ describe("JobcodeSingleSelectComponent", () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ JobcodeSingleSelectComponent ]
+      declarations: [ JobcodeSingleSelectComponent ],
+      imports: [
+        HttpClientTestingModule,
+        FormModule
+      ],
+      providers: [
+        AppConfig,
+        { provide: AuthService, useClass: AuthServiceStub },
+        { provide: KeywordSearchService, useClass: KeywordSearchServiceStub },
+      ]
     })
     .compileComponents()
+
+    const appConfig = TestBed.inject(AppConfig)
+    AppConfig.settings = appConfig.defaultConfig()  // This avoids the race condition on reading the config's whitelabel.toolName
   })
 
   beforeEach(() => {
@@ -21,5 +39,19 @@ describe("JobcodeSingleSelectComponent", () => {
 
   it("should create", () => {
     expect(component).toBeTruthy()
+  })
+
+  it("performSearch should work", () => {
+    const service = TestBed.inject(KeywordSearchService)
+    const spy = spyOn(service, "searchJobcodes").and.callThrough()
+    component.performSearch("jobcode 1")
+    expect(spy).toHaveBeenCalled()
+  })
+
+  it("performSearch should not be called", () => {
+    const service = TestBed.inject(KeywordSearchService)
+    const spy = spyOn(service, "searchJobcodes").and.callThrough()
+    component.performSearch("")
+    expect(spy).not.toHaveBeenCalled()
   })
 })

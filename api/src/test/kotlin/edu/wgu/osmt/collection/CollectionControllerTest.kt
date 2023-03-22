2ef580d2e32905bb7d55ab8880a621e18639706d
Jose Chavez <j.chavez@wgu.edu>
2023-03-22 16:55:42 -0600

    Adding Unit test for Updating the status of a Collection

--- api/src/test/kotlin/edu/wgu/osmt/collection/CollectionControllerTest.kt
+++ api/src/test/kotlin/edu/wgu/osmt/collection/CollectionControllerTest.kt
@@ -4,6 +4,7 @@ import edu.wgu.osmt.BaseDockerizedTest
 import edu.wgu.osmt.HasDatabaseReset
 import edu.wgu.osmt.HasElasticsearchReset
 import edu.wgu.osmt.SpringTest
+import edu.wgu.osmt.api.model.ApiCollectionUpdate
 import edu.wgu.osmt.config.AppConfig
 import edu.wgu.osmt.db.PublishStatus
 import edu.wgu.osmt.jobcode.JobCodeEsRepo
@@ -13,7 +14,13 @@ import edu.wgu.osmt.richskill.RichSkillEsRepo
 import org.assertj.core.api.Assertions
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
+import org.mockito.Mockito
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.security.core.Authentication
+import org.springframework.security.core.GrantedAuthority
+import org.springframework.security.core.context.SecurityContext
+import org.springframework.security.core.context.SecurityContextHolder
+import org.springframework.security.oauth2.core.user.OAuth2UserAuthority
 import org.springframework.security.oauth2.jwt.Jwt
 import org.springframework.test.util.ReflectionTestUtils
 import org.springframework.transaction.annotation.Transactional
@@ -31,6 +38,8 @@ internal class CollectionControllerTest @Autowired constructor(
     @Autowired
     lateinit var collectionController: CollectionController
 
+    var authentication: Authentication = Mockito.mock(Authentication::class.java)
+
     private lateinit var mockData : MockData
 
     val userString = "unittestuser"
@@ -42,6 +51,17 @@ internal class CollectionControllerTest @Autowired constructor(
     fun setup() {
         mockData = MockData()
         ReflectionTestUtils.setField(appConfig, "roleAdmin", "ROLE_Osmt_Admin")
+        val securityContext: SecurityContext = Mockito.mock(SecurityContext::class.java)
+        SecurityContextHolder.setContext(securityContext)
+
+        val attributes: MutableMap<String, Any> = HashMap()
+        attributes["email"] = userEmail
+
+        val authority: GrantedAuthority = OAuth2UserAuthority("ROLE_Osmt_Admin", attributes)
+        val authorities: MutableSet<GrantedAuthority> = HashSet()
+        authorities.add(authority)
+        Mockito.`when`(securityContext.authentication).thenReturn(authentication)
+        Mockito.`when`(SecurityContextHolder.getContext().authentication.authorities).thenReturn(authorities)
     }
 
     @Test
@@ -69,5 +89,89 @@ internal class CollectionControllerTest @Autowired constructor(
         // assert
         Assertions.assertThat(collectionRepository.findAll().toList()).hasSize(1)
         Assertions.assertThat(result).isNotNull
+    }
+
+    @Test
+    fun `updateCollection() should change the collection status when providing an Archived status and update the archived date`() {
+        // arrange
+        val jwt = Jwt.withTokenValue("foo").header("foo", "foo").claim("email", userEmail).build()
+        val update = ApiCollectionUpdate(
+            name = "newName",
+            description = "newDescription",
+            publishStatus = PublishStatus.Archived,
+            author = "newAuthor")
+        val collectionUUID = collectionRepository.create(name = "name", user = "user", email = "j.chavez@wgu.edu", description = "description")!!.uuid
+
+        // act
+        Assertions.assertThat(collectionRepository.findByUUID(collectionUUID)!!.status).isEqualTo(PublishStatus.Draft)
+        Assertions.assertThat(collectionRepository.findByUUID(collectionUUID)!!.archiveDate).isNull()
+        val result = collectionController.updateCollection(collectionUUID, update, jwt)
+
+        // assert
+        Assertions.assertThat(result).isNotNull
+        Assertions.assertThat(result.status).isEqualTo(PublishStatus.Archived)
+        Assertions.assertThat(result.archiveDate).isNotNull
+    }
+
+    @Test
+    fun `updateCollection() should return a collection to draft status when providing an Unarchived status and update the archived date`() {
+        // arrange
+        val jwt = Jwt.withTokenValue("foo").header("foo", "foo").claim("email", userEmail).build()
+        val update = ApiCollectionUpdate(
+            name = "newName",
+            description = "newDescription",
+            publishStatus = PublishStatus.Unarchived,
+            author = "newAuthor")
+        val collection = collectionRepository.create(name = "name", user = "user", email = "j.chavez@wgu.edu", description = "description")
+        collection!!.status = PublishStatus.Archived
+
+        // act
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.status).isEqualTo(PublishStatus.Archived)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.archiveDate).isNull()
+        val result = collectionController.updateCollection(collection.uuid, update, jwt)
+
+        // assert
+        Assertions.assertThat(result).isNotNull
+        Assertions.assertThat(result.status).isEqualTo(PublishStatus.Draft)
+        Assertions.assertThat(result.archiveDate).isNull()
+    }
+
+    @Test
+    fun `updateCollection() should return a collection to published status when providing an Unarchived status and update the archived and published date`() {
+        // arrange
+        val jwt = Jwt.withTokenValue("foo").header("foo", "foo").claim("email", userEmail).build()
+        var update = ApiCollectionUpdate(
+            name = "newName",
+            description = "newDescription",
+            publishStatus = PublishStatus.Published,
+            author = "newAuthor")
+        val collection = collectionRepository.create(name = "name", user = "user", email = "j.chavez@wgu.edu", description = "description")
+
+        // act
+        Assertions.assertThat(collectionRepository.findByUUID(collection!!.uuid)!!.status).isEqualTo(PublishStatus.Draft)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.archiveDate).isNull()
+        collectionController.updateCollection(collection.uuid, update, jwt)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.status).isEqualTo(PublishStatus.Published)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.publishDate).isNotNull()
+        update = ApiCollectionUpdate(
+            name = "newName",
+            description = "newDescription",
+            publishStatus = PublishStatus.Archived,
+            author = "newAuthor")
+        collectionController.updateCollection(collection.uuid, update, jwt)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.status).isEqualTo(PublishStatus.Archived)
+        Assertions.assertThat(collectionRepository.findByUUID(collection.uuid)!!.archiveDate).isNotNull
+        update = ApiCollectionUpdate(
+            name = "newName",
+            description = "newDescription",
+            publishStatus = PublishStatus.Unarchived,
+            author = "newAuthor")
+        val result = collectionController.updateCollection(collection.uuid, update, jwt)
+
+        // assert
+        Assertions.assertThat(result).isNotNull
+        Assertions.assertThat(result.status).isEqualTo(PublishStatus.Published)
+        Assertions.assertThat(result.archiveDate).isNull()
+        Assertions.assertThat(result.publishDate).isNotNull
     }
 }

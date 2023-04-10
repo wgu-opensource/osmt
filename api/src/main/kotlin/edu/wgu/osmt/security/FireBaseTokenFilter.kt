443cf5e98e5f0e39efa222e6a3cea1e0b33d0361
Jose Chavez <j.chavez@wgu.edu>
2023-04-10 13:18:17 -0600

    testing code for config fail

---
+++ api/src/main/kotlin/edu/wgu/osmt/security/FireBaseTokenFilter.kt
@@ -0,0 +1,18 @@
+package edu.wgu.osmt.security
+
+import org.springframework.web.filter.OncePerRequestFilter
+import java.io.IOException
+import javax.servlet.FilterChain
+import javax.servlet.ServletException
+import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
+
+
+//@Order(1)
+//@Component
+class FireBaseTokenFilter : OncePerRequestFilter() {
+    @Throws(ServletException::class, IOException::class)
+    override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {
+        //Filter Code Here
+    }
+}

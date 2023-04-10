8d18b844a85140fe4a9fe7d4f40911ed1eeac0f2
Jose Chavez <j.chavez@wgu.edu>
2023-04-10 13:48:26 -0600

    enabling order for filter component.

--- api/src/main/kotlin/edu/wgu/osmt/security/FireBaseTokenFilter.kt
+++ api/src/main/kotlin/edu/wgu/osmt/security/FireBaseTokenFilter.kt
@@ -1,5 +1,7 @@
 package edu.wgu.osmt.security
 
+import org.springframework.core.annotation.Order
+import org.springframework.stereotype.Component
 import org.springframework.web.filter.OncePerRequestFilter
 import java.io.IOException
 import javax.servlet.FilterChain
@@ -8,8 +10,8 @@ import javax.servlet.http.HttpServletRequest
 import javax.servlet.http.HttpServletResponse
 
 
-//@Order(1)
-//@Component
+@Order(1)
+@Component
 class FireBaseTokenFilter : OncePerRequestFilter() {
     @Throws(ServletException::class, IOException::class)
     override fun doFilterInternal(request: HttpServletRequest, response: HttpServletResponse, chain: FilterChain) {

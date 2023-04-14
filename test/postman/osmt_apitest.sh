8e229b0d2bcb98321803d6f47599647a1b12c367
Sowmya Chaganti <sowmya.chaganti@wgu.edu>
2023-04-14 10:30:50 -0400

    added newman installation as dev dependency n package.json file

--- test/postman/osmt_apitest.sh
+++ test/postman/osmt_apitest.sh
@@ -13,31 +13,24 @@ function setup_environment() {
 
 	#Setting up NodeJS
 	echo "Checking if NodeJS is installed ..."
-	if which node > /dev/null
+	  if which node > /dev/null
 		node -v
     then
-        echo "node is installed, skipping installation..."
-    else
-        echo "Installing node ..."
-        npm install n
-        n lts
+        echo "node is installed ..."
     fi
 
     #checking npm version
-    	echo "Checking npm version ..."
-    	npm --v
-
+    if which npm > /dev/null
+    		npm -v
+        then
+            echo "npm is installed ..."
+    fi
 
     #Setting up Newman
     echo "Checking if Newman is installed ..."
-	if newman -v
+	  if newman -v
     then
-        echo "Newman is installed, skipping..."
-    else
-        echo "Installing Newman ..."
-        npm install newman
-        newman --version
-        echo "Successfly installed Newman ... "
+        echo "Newman is installed ..."
     fi
 }
 

0cd8f2abc2ba9c014e9c1a3cb51189125efe61fc
Edwin Santizo <edwin.santizo@wgu.edu>
2023-04-17 17:10:19 -0600

    remvoed seperate function and added inline in main. Updated jps grep call

--- test/shutdown-testing.sh
+++ test/shutdown-testing.sh
@@ -2,23 +2,13 @@
 
 set -eu
 
-declare appPID
-
-_get_app_pid() {
-  appPID=($(jps | grep ApplicationKt))
-}
-
-_kill_spring_app() {
-  kill -SIGINT ${appPID}
-}
-
 main(){
   echo "Stopping spring app initialized..."
 
-  _get_app_pid
+  local appPID="$(jps -l | grep edu.wgu.osmt.ApplicationKt | awk '{print $1}')"
   echo "Got app PID ${appPID}"
 
-  _kill_spring_app
+  kill -SIGINT ${appPID}
   echo "Spring app has been stopped"
   #Need to add an error catcher
 }

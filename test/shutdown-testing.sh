4f40e7e52b2ec2b7eb4efeefed1cfc661a8c3cc6
Edwin Santizo <edwin.santizo@wgu.edu>
2023-04-17 13:43:30 -0600

    Graceful shutdown of spring app script created script that grabs spring app PID and sends SIGINT to app to stop it.

---
+++ test/shutdown-testing.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+set -eu
+
+declare appPID
+
+_get_app_pid() {
+  appPID=($(jps | grep ApplicationKt))
+}
+
+_kill_spring_app() {
+  kill -SIGINT appPID
+}
+
+main(){
+  echo "Stopping spring app initialized..."
+
+  _get_app_pid
+  echo "Got app PID"
+
+  _kill_spring_app
+  echo "Spring app has been stopped"
+  #Need to add an error catcher
+}

1fc7fed5d2eff7b57ab2e34b5a18151d485e5379
Edwin Santizo <edwin.santizo@wgu.edu>
2023-04-18 12:36:46 -0600

    Added check for verifying if app is running before trying to shut it down added a second check to verify that app is indeed stopped.

--- test/shutdown-testing.sh
+++ test/shutdown-testing.sh
@@ -5,12 +5,26 @@ set -eu
 main(){
   echo "Stopping spring app initialized..."
 
+  local retry_limit=5
+  local retry_count=0
   local appPID="$(jps -l | grep edu.wgu.osmt.ApplicationKt | awk '{print $1}')"
-  echo "Got app PID ${appPID}"
 
-  kill -SIGINT ${appPID}
-  echo "Spring app has been stopped"
-  #Need to add an error catcher
+  if [[ -z "${appPID}" ]]; then
+    echo "Application not running"
+    exit 1
+  else
+    echo "Got app PID ${appPID}"
+  fi
+
+      kill -SIGINT ${appPID}
+      sleep 1
+
+      if [[ -z "$(jps -l | grep edu.wgu.osmt.ApplicationKt | awk '{print $1}')" ]]; then
+        echo "Spring app has been stopped"
+      else
+        echo "App failed to stop"
+        exit 1
+      fi
 }
 
 main

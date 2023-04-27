b6c8b496b73784d92523f04ded04a31f6c06b1fe
Jose Chavez <115018284+jchavez137@users.noreply.github.com>
2023-04-27 09:13:36 -0600

    implementing curl retry to get index page of OSMT app, adding Static … (#370)
    * implementing curl retry to get index page of OSMT app, adding Static data set import call and Reindex
    
    * Graceful shutdown of spring app script (#365)
    
    * Graceful shutdown of spring app script
    created script that grabs spring app PID and sends SIGINT to app to stop it.
    
    * forgot to call the main function, added it back
    
    * remvoed seperate function and added inline in main. Updated jps grep call
    
    * Added check for verifying if app is running before trying to shut it down
    added a second check to verify that app is indeed stopped.
    
    * added a retry function
    extrapolated the get app pid into it's own function for DRY purposes.
    crated a retry function that tries 12 times and waits 3 secs in between.
    
    * adding call to shutdown instance script
    
    * adding call to shutdown instance script
    
    * moving shutdown script to bin folder, adding logic to stop api script
    
    * removing shutdown_osmt_app.sh and adding that very logic to stop_api_test_server.sh
    
    * removing unnecessary code in start_api_test_server.sh, removing unsafe murder of process, changing to be in a more safe way. Adding the call to stop the test stack in stop_api_test_server.sh.
    
    * Added a place for a file log called osmt_spring_app.log
    created trap for error and sigint to handle clean up
    
    * Incorporated feedback from John
    * renamed logFile to proper bash log_file
    * removed commented code
    * changed directory to /target where it can get destroyed after mvn clean install
    
    ---------
    
    Co-authored-by: wgu-edwin <68077442+wgu-edwin@users.noreply.github.com>
    Co-authored-by: Edwin Santizo <edwin.santizo@wgu.edu>

--- test/bin/stop_api_test_server.sh
+++ test/bin/stop_api_test_server.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 # shellcheck disable=SC2181
 
-set -x
-set -u
+set -eu
 
 export DEBUG=1
 export OSMT_STACK_NAME=osmt_api_test
 
 declare project_dir
+declare -r OSMT_APP_CLASS='edu.wgu.osmt.ApplicationKt'
 
 _get_osmt_project_dir() {
   local project_dir; project_dir="$(git rev-parse --show-toplevel 2> /dev/null)"
@@ -19,10 +19,51 @@ _get_osmt_project_dir() {
   echo "${project_dir}"
 }
 
+
+get_app_pid(){
+  local app_pid; app_pid="$(jps -l | grep ${OSMT_APP_CLASS} | awk '{print $1}')"
+
+  echo "${app_pid}"
+}
+
+check_pid_status_retry(){
+  for retry_limit in {12..0}; do
+    if [[ -z "$(get_app_pid)" ]]; then
+      echo "INFO: Application ${OSMT_APP_CLASS} successfully stopped. Exiting..."
+      exit 0
+    fi
+    echo "INFO: Could not stop application ${OSMT_APP_CLASS}. Will retry ${retry_limit} more times. Retrying in 3 seconds..."
+    local app_pid; app_pid="$(jps -l | grep ${OSMT_APP_CLASS} | awk '{print $1}')"
+    echo "${app_pid}"
+    sleep 3
+  done
+
+  echo "ERROR: Could not stop ${OSMT_APP_CLASS} after 12 retries. Exiting..." 1>&2;
+  exit 135
+}
+
+shutdown_osmt_app(){
+  echo "INFO: Attempting to stop application ${OSMT_APP_CLASS}."
+  local app_pid; app_pid="$(get_app_pid)"
+
+  if [[ -z "${app_pid}" ]]; then
+    echo "ERROR: Application ${OSMT_APP_CLASS} not running" 1>&2;
+    exit 135
+  else
+    echo "INFO: Application ${OSMT_APP_CLASS} found PID ${app_pid}."
+  fi
+
+  kill -SIGTERM "${app_pid}"
+
+  check_pid_status_retry
+}
+
 project_dir="$(_get_osmt_project_dir)" || exit 135
 
 # stop the API test server
+shutdown_osmt_app
 
 # clean up API test Docker resources
+"${project_dir}/osmt_cli.sh" -e || exit 135
 
 

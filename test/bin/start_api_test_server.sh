f0ecade937d33c36e5d37be1b97211925d281404
Jose Chavez <115018284+jchavez137@users.noreply.github.com>
2023-04-27 09:13:36 -0600

    implementing curl retry to get index page of OSMT app, adding Static … (#370)
    * implementing curl retry to get index page of OSMT app, adding Static data set import call and Reindex
    
    * Graceful shutdown of spring app script (#365)
    
    * Graceful shutdown of spring app script
    created script that grabs spring app PID and sends SIGINT to app to stop it.
    
    * forgot to call the main function, added it back
    
    * remvoed seperate function and added inline in main. Updated jps grep call
    
    * Added check for verifying if app is running before trying to shut it down
    added a second check to verify that app is indeed stopped.
    
    * added a retry function
    extrapolated the get app pid into it's own function for DRY purposes.
    crated a retry function that tries 12 times and waits 3 secs in between.
    
    * adding call to shutdown instance script
    
    * adding call to shutdown instance script
    
    * moving shutdown script to bin folder, adding logic to stop api script
    
    * removing shutdown_osmt_app.sh and adding that very logic to stop_api_test_server.sh
    
    * removing unnecessary code in start_api_test_server.sh, removing unsafe murder of process, changing to be in a more safe way. Adding the call to stop the test stack in stop_api_test_server.sh.
    
    * Added a place for a file log called osmt_spring_app.log
    created trap for error and sigint to handle clean up
    
    * Incorporated feedback from John
    * renamed logFile to proper bash log_file
    * removed commented code
    * changed directory to /target where it can get destroyed after mvn clean install
    
    ---------
    
    Co-authored-by: wgu-edwin <68077442+wgu-edwin@users.noreply.github.com>
    Co-authored-by: Edwin Santizo <edwin.santizo@wgu.edu>

--- test/bin/start_api_test_server.sh
+++ test/bin/start_api_test_server.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 # shellcheck disable=SC2181
 
-set -x
-set -u
+#set -x
+set -eu
 
 export DEBUG=1
 export OSMT_STACK_NAME=osmt_api_test
@@ -15,13 +15,12 @@ export ELASTICSEARCH_TRANSPORT_PORT="29300"
 export ELASTICSEARCH_URI="localhost:${ELASTICSEARCH_HTTP_PORT}"
 export REDIS_PORT="26379"
 export REDIS_URI="localhost:${REDIS_PORT}"
-export APP_PORT="28080"
+export APP_PORT="8080"
 export BASE_DOMAIN="localhost:${APP_PORT}"
 export BASE_URL="http://${BASE_DOMAIN}"
 export OSMT_FRONT_END_PORT="${APP_PORT}"
 
-
-declare project_dir
+declare -ri LOAD_CI_DATASET="${LOAD_CI_DATASET:-0}"
 
 _get_osmt_project_dir() {
   local project_dir; project_dir="$(git rev-parse --show-toplevel 2> /dev/null)"
@@ -33,17 +32,67 @@ _get_osmt_project_dir() {
   echo "${project_dir}"
 }
 
-project_dir="$(_get_osmt_project_dir)" || exit 135
+curl_with_retry() {
+  local -i rc=-1
+  local -i retry_limit=12
+  until [ ${rc} -eq 0 ] && [ ${retry_limit} -eq 0 ]; do
+      echo_info "Attempting to request the index page of the OSMT Spring app with curl..."
+      curl -s "${BASE_URL}" 1>/dev/null 2>/dev/null
+      rc=$?
+      if [[ ${rc} -eq 0 ]]; then
+        echo_info "Index page loaded. Proceeding..."
+        return 0
+      fi
+      if [[ ${retry_limit} -eq 0 ]]; then
+        echo_err "Could not load the index page."
+        return 1
+      fi
+      if [[ ${rc} -ne 0 ]]; then
+        echo_info "Could not load the index page. Will retry ${retry_limit} more times. Retrying in 10 seconds..."
+      fi
+      let retry_limit-=1
+      sleep 10
+  done
+}
 
-# start the API test  Docker compose stack and Spring app server
-"${project_dir}/osmt_cli.sh" -s
 
-# load CI static dataset
-#"${project_dir}/osmt_cli.sh" -l
+echo_info() {
+  echo "INFO: $*"
+}
 
-# reindex ElasticSearch
-#"${project_dir}/osmt_cli.sh" -r
+echo_err() {
+  echo "ERROR: $*" 1>&2;
+}
 
+error_handler() {
+  echo_err "Trapping at error_handler. Exiting"
+  # clean up API test Docker resources
+  "${project_dir}/osmt_cli.sh" -e || exit 135
+}
 
+main() {
+  local project_dir; project_dir="$(_get_osmt_project_dir)" || exit 135
+  local log_file; log_file="${project_dir}/api/target/osmt_spring_app.log"
 
+  # start the API test Docker compose stack and Spring app server, detached. Send log files to 'osmt_spring_app.log'
+  echo_info "Starting OSMT Spring app for ${OSMT_STACK_NAME}. Output is suppressed, because console is detached."
+  echo_info "See 'osmt_spring_app.log' for console output. Proceeding..."
 
+  touch "$log_file"
+  "${project_dir}/osmt_cli.sh" -s  1>"$log_file" 2>"$log_file" & disown  || exit 135
+
+  # curl the Spring app and retry for 2 minutes
+  curl_with_retry || exit 135
+  
+  if [[ ${LOAD_CI_DATASET} -eq 0  ]]; then
+    # load CI static dataset
+    "${project_dir}/osmt_cli.sh" -l || exit 135
+
+    # reindex ElasticSearch
+    "${project_dir}/osmt_cli.sh" -r || exit 135
+  fi
+}
+
+trap error_handler ERR SIGINT SIGTERM
+
+main

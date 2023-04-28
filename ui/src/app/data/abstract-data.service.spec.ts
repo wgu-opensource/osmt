1940a46c8a1d74b358906628f0f10a50add56f2a
Jose Chavez <j.chavez@wgu.edu>
2023-04-28 09:51:59 -0600

    adding providers for unit testing in abstract-data.service.spec.ts.

--- ui/src/app/data/abstract-data.service.spec.ts
+++ ui/src/app/data/abstract-data.service.spec.ts
@@ -1,16 +1,33 @@
 import { TestBed } from "@angular/core/testing"
 
 import { AbstractDataService } from "./abstract-data.service"
+import {HttpClientTestingModule} from "@angular/common/http/testing"
+import {EnvironmentService} from "../core/environment.service"
+import {AppConfig} from "../app.config"
+import {NamedReferenceService} from "./named-reference.service"
+import {Location} from "@angular/common"
+import {AuthService} from "../auth/auth-service"
+import {AuthServiceStub, RouterStub} from "../../../test/resource/mock-stubs"
+import {Router} from "@angular/router"
 
 describe("AbstractAdminService", () => {
-  let service: AbstractDataService
+  let testService: AbstractDataService
 
   beforeEach(() => {
-    TestBed.configureTestingModule({})
-    service = TestBed.inject(AbstractDataService)
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        EnvironmentService,
+        AppConfig,
+        AbstractDataService,
+        Location,
+        { provide: AuthService, useClass: AuthServiceStub },
+        { provide: Router, useClass: RouterStub }
+      ]})
+    testService = TestBed.inject(AbstractDataService)
   })
 
   it("should be created", () => {
-    expect(service).toBeTruthy()
+    expect(testService).toBeTruthy()
   })
 })

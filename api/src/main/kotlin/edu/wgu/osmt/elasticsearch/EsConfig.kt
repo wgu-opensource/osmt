de35f80011f60dbc2e4a367a1e38e1c25069be9d
Huey <shnooga@gmail.com>
2023-05-08 15:47:43 -0700

    DMND-1307 Adding Redis basic auth for osmtLib api v2.5.2

--- api/src/main/kotlin/edu/wgu/osmt/elasticsearch/EsConfig.kt
+++ api/src/main/kotlin/edu/wgu/osmt/elasticsearch/EsConfig.kt
@@ -5,8 +5,4 @@ import org.springframework.boot.context.properties.ConstructorBinding
 
 @ConstructorBinding
 @ConfigurationProperties(prefix = "es", ignoreInvalidFields = true)
-data class EsConfig(
-    val uri: String,
-    val login: String,
-    val password: String
-    )
+data class EsConfig(val uri: String)

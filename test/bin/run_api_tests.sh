d57a6c35a26aa7d3fbeb5a1872a63a83edc0bd6e
Edwin Santizo <edwin.santizo@wgu.edu>
2023-05-09 09:32:30 -0600

    Added the get_bearer_token functionality part of the run script now when run api tests it has the token to do so.

--- test/bin/run_api_tests.sh
+++ test/bin/run_api_tests.sh
@@ -3,6 +3,7 @@
 set -eu
 
 declare apitest_env_file
+declare bearer_token
 
 declare OKTA_URL
 declare OKTA_USERNAME
@@ -10,47 +11,52 @@ declare OKTA_PASSWORD
 
 _get_osmt_project_dir() {
   local project_dir; project_dir="$(git rev-parse --show-toplevel 2> /dev/null)"
-  echo "${project_dir}"
+  echo "$project_dir"
 }
 
 function source_osmt_apitest_env_file() {
-  echo "Sourcing ${apitest_env_file}"
+  echo "Sourcing $apitest_env_file"
   set -o allexport
-  source "${apitest_env_file}"
+  # shellcheck source="test/osmt-apitest.env"
+  source "$apitest_env_file"
   set +o allexport
 }
 
 function get_bearer_token() {
-  declare bearer_token
-  declare auth_env; auth_env="${test_dir}/postman/osmt-auth.environment.json"
+  declare auth_env; auth_env="$test_dir/postman/osmt-auth.environment.json"
 
 	# Running postman collections
 	echo "Getting access token from OKTA ..."
-  newman run "${test_dir}/postman/osmt-auth.postman_collection.json" \
-    --env-var oktaUsername="$OKTA_USERNAME" \
-    --env-var oktaPassword="$OKTA_PASSWORD" \
-    --env-var oktaUrl="$OKTA_URL" \
-    --env-var baseUrl="$BASE_URL" \
-    --ignore-redirects \
-    --export-environment "$auth_env"
+  npx "$test_dir/node_modules/.bin/newman" \
+    run "$test_dir/postman/osmt-auth.postman_collection.json" \
+      --env-var oktaUsername="$OKTA_USERNAME" \
+      --env-var oktaPassword="$OKTA_PASSWORD" \
+      --env-var oktaUrl="$OKTA_URL" \
+      --env-var baseUrl="$BASE_URL" \
+      --ignore-redirects \
+      --export-environment "$auth_env"
 
   bearer_token="$(jq '.["values"][] | select(.key=="bearerToken") | .value' "$auth_env")"
   echo "Retrieved token:"
   echo "$bearer_token"
 
-  echo "bearerToken=$bearer_token" > "./token.env"
+  echo "bearerToken=$bearer_token" > "$test_dir/postman/token.env"
   echo
 }
 
 run_api_tests() {
+  echo "Running postman collection ..."
+  # Removing leading and trailing double quotes.
+  bearer_token="${bearer_token%\"}"
+  bearer_token="${bearer_token#\"}"
   npx "$test_dir/node_modules/.bin/newman" \
     run "$test_dir/postman/osmt.postman_collection.json" \
-    --env-var baseUrl="$BASE_URL"
+      --env-var baseUrl="$BASE_URL" \
+      --env-var bearerToken="$bearer_token"
 }
 
-
 test_dir="$(_get_osmt_project_dir)/test" || exit 135
-apitest_env_file="${test_dir}/osmt-apitest.env"
+apitest_env_file="$test_dir/osmt-apitest.env"
 
 source_osmt_apitest_env_file
 get_bearer_token

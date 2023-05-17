4bfa506ad950717cdad086485384e8891a4abfee
Jose Chavez <115018284+jchavez137@users.noreply.github.com>
2023-05-17 15:58:38 -0600

    OSMT- 276 service classes UI (#391)
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one.
    
    * refactoring JobCode classes and package. Adding CRUD requests
    
    * adding providers for unit testing in abstract-data.service.spec.ts
    
    * Adding Enum for MetadataType, Optimizing Imports
    
    * Adding implementation for delete method in Abstract service
    
    * Remove unused code
    
    - Remove ApiGetParams.
    
    - Constructor in abstract.service now is protected.
    
    * Add alias for test directory
    
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one
    
    * updating openapi.yaml to match endpoint definition
    
    * Correcting Routepath and refactoring web methods
    
    * Correcting buildTableParams sort param
    
    * Updating endpoints call to match definition
    
    ---------
    
    Co-authored-by: manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>

---
+++ ui/src/app/data/abstract-data.service.ts
@@ -0,0 +1,37 @@
+import { Location } from "@angular/common"
+import { HttpClient, HttpResponse } from "@angular/common/http"
+import { Injectable } from "@angular/core"
+import { Router } from "@angular/router"
+import { Observable } from "rxjs"
+import { share } from "rxjs/operators"
+import { AbstractService, ApiGetParams } from "../abstract.service"
+import { AuthService } from "../auth/auth-service"
+
+@Injectable({ providedIn: "root" })
+export abstract class AbstractDataService extends AbstractService {
+
+  protected constructor(httpClient: HttpClient, authService: AuthService, router: Router, location: Location) {
+    super(httpClient, authService, router, location)
+  }
+
+  /**
+   * Perform a patch request.
+   *
+   *   const {body, headers, status, type, url} = response
+   *
+   * @param path The relative path to the endpoint
+   * @param headers Json blob defining headers
+   * @param params Json blob defining path params
+   * @param body Json blob defining the changes to be applied to the object
+   */
+  patch<T>({path, headers, params, body}: ApiGetParams): Observable<HttpResponse<T>> {
+    const observable =  this.httpClient.patch<T>(this.buildUrl(path + "/update"), body, {
+      headers: this.wrapHeaders(headers),
+      params,
+      observe: "response"}).pipe(share())
+    observable
+      .subscribe(() => {}, (err) => { this.redirectToLogin(err) })
+    return observable
+  }
+
+}

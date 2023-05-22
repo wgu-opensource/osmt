1e8cf22630f5bf464cdc7c2d1527abcf74dc2466
manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
2023-05-22 11:55:11 -0600

    Update to angular 16

--- ui/src/app/auth/auth.guard.ts
+++ ui/src/app/auth/auth.guard.ts
@@ -1,11 +1,11 @@
 import {Injectable} from "@angular/core"
-import {ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot} from "@angular/router"
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from "@angular/router"
 import {ENABLE_ROLES} from "./auth-roles"
 import {AuthService} from "./auth-service"
 import {ToastService} from "../toast/toast.service"
 
 @Injectable()
-export class AuthGuard implements CanActivate {
+export class AuthGuard  {
   constructor(private router: Router, protected toastService: ToastService, private authService: AuthService) {
 
   }

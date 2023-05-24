53a2681d6d98594a1a91b7dd548fe62d1b380387
Jose Chavez <115018284+jchavez137@users.noreply.github.com>
2023-05-24 08:55:10 -0600

    Fix after rebase onto feature/admin-screens
    Search/Filter/Delete Metadata Keywords(Named References) Enabling (#426)
    
    * Refactoring Controller to accept keyword type
    
    * Renaming Keyword RoutePaths
    
    * Addind overloaded typeAheadSearch method with pageable
    
    * Working on UI, adding service endpoint calls, adding service to NR table components.
    
    * Converting delete process to be task result, adjusting labels for plural category
    
    * removing unused named reference service, fixing enum issue, fixing case insensitive sort..
    
    * adding unit tests
    
    * Removing unused code for category screens, updating API yaml file
    
    * Removing hardcoded totalPageCount for named reference table, removing unused imports.
    
    * Reverting skillCount field back, fixing some PR comments, updating openapi.yaml
    
    * Removing unused imports.
    
    * Removing unused imports.
    
    * Refactoring deletion logic, fixing mock data.
    
    * Removing comented out line
    
    * Removing unused RoutePath
    
    * Add postman tests for job codes
    
    * Adding test coverage scripts
    
    * Adding test coverage scripts
    
    * Adding test coverage scripts
    
    * Correcting scripts.
    
    * Removing category script tests, No more needed.
    
    * Fix job code tests
    
    * Add pre request in id folder
    
    * Fix get pagination
    
    - Remove query parameter
    
    * Updating update keyword test.
    
    ---------
    
    Co-authored-by: manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
    
    OSMT- 276 service classes UI (#391)
    
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one.
    
    * refactoring JobCode classes and package. Adding CRUD requests
    
    * adding providers for unit testing in abstract-data.service.spec.ts
    
    * Adding Enum for MetadataType, Optimizing Imports
    
    * Adding implementation for delete method in Abstract service
    
    * Remove unused code
    
    - Remove ApiGetParams.
    
    - Constructor in abstract.service now is protected.
    
    * Add alias for test directory
    
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one
    
    * updating openapi.yaml to match endpoint definition
    
    * Correcting Routepath and refactoring web methods
    
    * Correcting buildTableParams sort param
    
    * Updating endpoints call to match definition
    
    ---------
    
    Co-authored-by: manuel-delvillar <68391066+manuel-delvillar@users.noreply.github.com>
    
    Adding Screen to Search/Filter Metadata (#401)
    
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one.
    
    * refactoring JobCode classes and package. Adding CRUD requests
    
    * Adding Enum for MetadataType, Optimizing Imports
    
    * Adding implementation for delete method in Abstract service
    
    * Adding new services classes for jobCodes and namedReferences
    
    * Renaming package and services name classes. Extending new abstract class from the existing one.
    
    * refactoring JobCode classes and package. Adding CRUD requests
    
    * Adding Enum for MetadataType, Optimizing Imports
    
    * updating openapi.yaml to match endpoint definition.
    
    * Correcting Routepath and refactoring web methods
    
    * Refactoring entry points in navigation bar

---
+++ ui/src/app/metadata/abstract-data.service.spec.ts
@@ -0,0 +1,36 @@
+import { TestBed } from "@angular/core/testing"
+import { Router } from "@angular/router"
+import { HttpClientTestingModule } from "@angular/common/http/testing"
+import { Location } from "@angular/common"
+import { AbstractDataService } from "./abstract-data.service"
+import { EnvironmentService } from "../core/environment.service"
+import { AppConfig } from "../app.config"
+import { AuthService } from "../auth/auth-service"
+import { AuthServiceStub, RouterStub } from "@test/resource/mock-stubs"
+import { getBaseApi } from "../api-versions"
+
+describe("AbstractAdminService", () => {
+  let testService: AbstractDataService
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [
+        EnvironmentService,
+        AppConfig,
+        AbstractDataService,
+        Location,
+        { provide: AuthService, useClass: AuthServiceStub },
+        { provide: Router, useClass: RouterStub },
+        {
+          provide: "BASE_API",
+          useFactory: getBaseApi,
+        }
+      ]})
+    testService = TestBed.inject(AbstractDataService)
+  })
+
+  it("should be created", () => {
+    expect(testService).toBeTruthy()
+  })
+})

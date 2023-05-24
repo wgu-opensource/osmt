51c4db4abbdc75f5595239c6c51ecb313ffbd65e
Edwin Santizo <edwin.santizo@wgu.edu>
2023-05-24 16:28:22 -0600

    Added docker clean up
    * Added the clean up of docker stack at the beginning in the pre-test-setup. That way it doesn't destroy and possible logs in case tests fail
    
    * Fixed typo in the osmt.env.example so it is the correct variable name

--- test/bin/pre_test_setup.sh
+++ test/bin/pre_test_setup.sh
@@ -74,6 +74,30 @@ curl_with_retry() {
   done
 }
 
+clean_docker_stack() {
+  local project_dir; project_dir="$(_get_osmt_project_dir)" || exit 135
+  echo_info "Stopping OSMT ${OSMT_STACK_NAME} Docker stack"
+  cd "${project_dir}/docker" || return 1
+  docker-compose --file dev-stack.yml --project-name "${OSMT_STACK_NAME}" down
+  rc=$?
+  if [[ $rc -ne 0 ]]; then
+    echo_err "Stopping OSMT ${OSMT_STACK_NAME} Docker stack failed. Exiting..."
+    return 1
+  fi
+
+  echo
+  echo_info "Removing OSMT-related Docker containers..."
+  docker ps -aq --filter=name='osmt_api*' | xargs docker rm
+
+  echo
+  echo_info "Removing OSMT-related Docker images..."
+  docker images -q --filter=reference='osmt_api*' | xargs docker rmi
+
+  echo
+  echo_info "Removing OSMT-related Docker volumes..."
+  docker volume ls -q -f name=osmt_api | xargs docker volume rm -f {} &> /dev/null
+}
+
 
 echo_info() {
   echo "INFO: $*"
@@ -93,6 +117,12 @@ error_handler() {
 main() {
   local project_dir; project_dir="$(_get_osmt_project_dir)" || exit 135
   local log_file; log_file="${project_dir}/api/target/osmt_spring_app.log"
+
+  # Adding docker clean up at the beginning to keep previous test state
+  # in case we need to debug and cleaning up docker could remove logs.
+  if [[ -n $(docker ps -aq --filter=name="osmt_api*") ]]; then
+    clean_docker_stack
+  fi
 
   # Run NPM install
   echo_info "Installing NPM modules..."

6ca6a73f862201004e0c7adc321947e720409e23
Jose Chavez <j.chavez@wgu.edu>
2023-05-25 12:20:08 -0600

    Refactoring constants location

--- api/src/main/kotlin/edu/wgu/osmt/util/OsmtUtil.kt
+++ api/src/main/kotlin/edu/wgu/osmt/util/OsmtUtil.kt
@@ -1,26 +1,20 @@
 package edu.wgu.osmt.util
 
+import edu.wgu.osmt.config.CLOSING_BRACKET
+import edu.wgu.osmt.config.COMMA
+import edu.wgu.osmt.config.OPENING_BRACKET
+import edu.wgu.osmt.config.SEMICOLON
 import org.apache.commons.lang3.StringUtils
 
 class OsmtUtil {
 
-
     companion object {
-        private const val OPENING_BRACKET = "["
-        private const val CLOSING_BRACKET = "]"
-        private const val COMMA = ","
-        private const val SEMICOLON = ";"
-
         fun parseMultiValueToSingleValue(field: String) : String {
             return StringUtils.replace(
                     StringUtils.replace(
                             StringUtils.replace(field, OPENING_BRACKET, StringUtils.EMPTY),
-                            COMMA.plus(CLOSING_BRACKET), StringUtils.EMPTY
-                    ),
-                    COMMA.plus(COMMA).plus(StringUtils.SPACE), SEMICOLON
-            )
+                            COMMA.plus(CLOSING_BRACKET), StringUtils.EMPTY),
+                    COMMA.plus(COMMA).plus(StringUtils.SPACE), SEMICOLON)
         }
-
-
     }
 }

2db03e434361d7990df68b87c4638f52d6d5487e
Jose Chavez <j.chavez@wgu.edu>
2023-05-24 21:30:31 -0600

    Refactoring util method to use constants

--- api/src/main/kotlin/edu/wgu/osmt/util/OsmtUtil.kt
+++ api/src/main/kotlin/edu/wgu/osmt/util/OsmtUtil.kt
@@ -4,15 +4,23 @@ import org.apache.commons.lang3.StringUtils
 
 class OsmtUtil {
 
+
     companion object {
+        private const val OPENING_BRACKET = "["
+        private const val CLOSING_BRACKET = "]"
+        private const val COMMA = ","
+        private const val SEMICOLON = ";"
+
         fun parseMultiValueToSingleValue(field: String) : String {
             return StringUtils.replace(
                     StringUtils.replace(
-                            StringUtils.replace(
-                                    field, "[", StringUtils.EMPTY
-                            ), ",]", StringUtils.EMPTY
-                    ), ",, ", ";"
+                            StringUtils.replace(field, OPENING_BRACKET, StringUtils.EMPTY),
+                            COMMA.plus(CLOSING_BRACKET), StringUtils.EMPTY
+                    ),
+                    COMMA.plus(COMMA).plus(StringUtils.SPACE), SEMICOLON
             )
         }
+
+
     }
 }

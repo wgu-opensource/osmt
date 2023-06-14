7629161f036c5063f00ce350b5fff21653bb3305
Jose Chavez <j.chavez@wgu.edu>
2023-06-14 07:06:05 -0600

    Fixing logic mapping author and Cat List

--- api/src/main/kotlin/edu/wgu/osmt/api/model/ApiSkillUpdateMapper.kt
+++ api/src/main/kotlin/edu/wgu/osmt/api/model/ApiSkillUpdateMapper.kt
@@ -9,40 +9,51 @@ class ApiSkillUpdateMapper {
     companion object {
         //TODO: test Coverage
         fun mapApiSkillUpdateV2ToApiSkillUpdate(
-            apiSkillUpdateV2: ApiSkillUpdateV2,
-            skillUUID: String,
-            richSkillRepository: RichSkillRepository
-        ) : ApiSkillUpdate {
+                apiSkillUpdateV2: ApiSkillUpdateV2,
+                skillUUID: String,
+                richSkillRepository: RichSkillRepository
+        ): ApiSkillUpdate {
             return ApiSkillUpdate(
-                skillName = apiSkillUpdateV2.skillName,
-                skillStatement = apiSkillUpdateV2.skillStatement,
-                publishStatus = apiSkillUpdateV2.publishStatus,
-                collections = apiSkillUpdateV2.collections,
-                authors = getDiff(apiSkillUpdateV2.author?.split(SEMICOLON), skillUUID, richSkillRepository, KeywordTypeEnum.Author),
-                categories = getDiff(apiSkillUpdateV2.author?.split(SEMICOLON), skillUUID, richSkillRepository, KeywordTypeEnum.Category)
+                    skillName = apiSkillUpdateV2.skillName,
+                    skillStatement = apiSkillUpdateV2.skillStatement,
+                    publishStatus = apiSkillUpdateV2.publishStatus,
+                    collections = apiSkillUpdateV2.collections,
+                    authors = getDiff(apiSkillUpdateV2.author?.split(SEMICOLON), skillUUID, richSkillRepository, KeywordTypeEnum.Author),
+                    categories = getDiff(apiSkillUpdateV2.category?.split(SEMICOLON), skillUUID, richSkillRepository, KeywordTypeEnum.Category)
             )
         }
-        
+
         private fun getDiff(
-            split: List<String>?,
-            skillUUID: String,
-            richSkillRepository: RichSkillRepository,
-            type: KeywordTypeEnum
-        ): ApiStringListUpdate? {
-            var add: List<String>? = null
-            var remove: List<String>? = null
-            val storedList = richSkillRepository.findByUUID(skillUUID)?.keywords?.filter { it.type==type }?.mapNotNull { it.value }
-            
-            if(split!=null && storedList!=null){
-                if(split.filterNot { storedList.contains(it) }.isNotEmpty()) {
-                    add = split.filterNot { storedList.contains(it) }
-                }
-                if(storedList.filterNot { split.contains(it) }.isNotEmpty()) {
-                    remove = storedList.filterNot { split.contains(it) }
-                }
+                split: List<String>?,
+                skillUUID: String,
+                richSkillRepository: RichSkillRepository,
+                type: KeywordTypeEnum
+        ): ApiStringListUpdate {
+            val storedList = richSkillRepository.findByUUID(skillUUID)?.keywords?.filter { it.type == type }?.mapNotNull { it.value }
+
+            return if (split != null && storedList != null) {
+                getUncommon(split, storedList)
+            } else {
+                ApiStringListUpdate(listOf(), listOf())
             }
-            
+        }
+
+        private fun getUncommon(split: List<String>, stored: List<String>): ApiStringListUpdate {
+            val sum = split + stored
+            var add: List<String> = listOf()
+            var remove: List<String> = listOf()
+            if(split.size >= stored.size){
+                add = sum.groupBy { it }
+                        .filter { it.value.size == 1 }
+                        .flatMap { it.value }
+            }else {
+                remove = sum.groupBy { it }
+                        .filter { it.value.size == 1 }
+                        .flatMap { it.value }
+            }
+
             return ApiStringListUpdate(add, remove)
         }
     }
+
 }

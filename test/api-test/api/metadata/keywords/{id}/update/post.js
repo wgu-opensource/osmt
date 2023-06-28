87b61c14ac155d79d46a2bec2c8ecb4e12f099c1
Jose Chavez <j.chavez@wgu.edu>
2023-06-28 12:10:01 -0600

    Updating update keyword test.

--- test/api-test/api/metadata/keywords/{id}/update/post.js
+++ test/api-test/api/metadata/keywords/{id}/update/post.js
@@ -1,17 +1,22 @@
 // let expectedData is injected from <response-type>.json
 
 let responseData = pm.response.json();
-console.log("Check a keyword update");
+console.log("Check updated keyword");
 
-pm.test(`Keyword - Check object type`, function () {
-  pm.expect(responseData.type).to.equal(expected.type);
+pm.test("Check id match", function() {
+  pm.expect(responseData.id).to.equal(expectedData.id);
 });
-pm.test(`Keyword - Check name`, function () {
-  pm.expect(responseData.name).to.equal(expected.name);
+pm.test("Check name match", function() {
+  pm.expect(responseData.name).to.equal(expectedData.name);
 });
-pm.test(`Keyword - Check ID exists`, function () {
-  pm.expect(responseData.id).exists;
+pm.test("Check framework match", function() {
+  pm.expect(responseData.framework).to.equal(expectedData.framework);
 });
-pm.test(`Keyword - Check framework`, function () {
-  pm.expect(responseData.framework).to.equal(expected.framework);
+pm.test("Check url match", function() {
+  pm.expect(responseData.url).to.equal(expectedData.url);
 });
+pm.test("Check type match", function() {
+  pm.expect(responseData.type).to.equal(expectedData.type);
+});
+
+

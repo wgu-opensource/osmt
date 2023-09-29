f8287ae903b5bc8c8342127f6e17b0f588e8f176
Huey <shnooga@gmail.com>
2023-09-29 16:19:10 -0700

    Bug fix, using StringQuery(str) for old ES 7.x causes the the search template to ignore the filter property; fixed for FindsAllByPublishStatus but needs to update the more complex CollectionEsRepo and RichSkilsEsRepo.

--- api/src/main/kotlin/edu/wgu/osmt/elasticsearch/FindsAllByPublishStatus.kt
+++ api/src/main/kotlin/edu/wgu/osmt/elasticsearch/FindsAllByPublishStatus.kt
@@ -1,21 +1,18 @@
 package edu.wgu.osmt.elasticsearch
 
+import co.elastic.clients.elasticsearch._types.FieldValue
+import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders.*
+import co.elastic.clients.elasticsearch._types.query_dsl.TermsQueryField
 import edu.wgu.osmt.db.PublishStatus
-import org.elasticsearch.index.query.BoolQueryBuilder
-import org.elasticsearch.index.query.QueryBuilders
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.data.domain.Pageable
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchTemplate
+import org.springframework.data.elasticsearch.client.elc.NativeQueryBuilder
 import org.springframework.data.elasticsearch.client.erhlc.NativeSearchQueryBuilder
 import org.springframework.data.elasticsearch.core.SearchHits
 import org.springframework.data.elasticsearch.core.query.Query
 import org.springframework.data.elasticsearch.core.query.StringQuery
-
-import org.springframework.data.elasticsearch.client.elc.*
-import co.elastic.clients.elasticsearch._types.FieldValue
-import co.elastic.clients.elasticsearch._types.query_dsl.QueryBuilders.*
-import co.elastic.clients.elasticsearch._types.query_dsl.TermsQueryField
 import java.util.stream.Collectors
 
 interface FindsAllByPublishStatus<T> {
@@ -23,66 +20,15 @@ interface FindsAllByPublishStatus<T> {
     val javaClass: Class<T>
 
     fun findAllFilteredByPublishStatus(publishStatus: Set<PublishStatus>, pageable: Pageable): SearchHits<T> {
-        val nqb = buildQuery(pageable, publishStatus)
-        val query = createStringQuery(
-            "FindsAllByPublishStatus.findAllFilteredByPublishStatus()",
-            nqb,
-            LoggerFactory.getLogger(FindsAllByPublishStatus::class.java)
-        )
-        return elasticSearchTemplate.search(query, javaClass)
-
-        /* TODO: Uncomment to Upgrade to ElasticSearch 8.x apis
         val nqb = createQueryBuilder(pageable, publishStatus)
-        val query = createQuery("FindsAllByPublishStatus.countAllFilteredByPublishStatus()", nqb, LoggerFactory.getLogger(FindsAllByPublishStatus::class.java))
+        val query = createQuery("FindsAllByPublishStatus.findAllFilteredByPublishStatus()", nqb, LoggerFactory.getLogger(FindsAllByPublishStatus::class.java))
         return elasticSearchTemplate.search(query, javaClass)
-         */
     }
 
     fun countAllFilteredByPublishStatus(publishStatus: Set<PublishStatus>, pageable: Pageable): Long {
-        val nqb = buildQuery(pageable, publishStatus)
-        val query = createStringQuery(
-            "FindsAllByPublishStatus.countAllFilteredByPublishStatus()",
-            nqb,
-            LoggerFactory.getLogger(FindsAllByPublishStatus::class.java)
-        )
-        return elasticSearchTemplate.count(query, javaClass)
-
-        /* TODO: Uncomment to Upgrade to ElasticSearch 8.x apis
         val nqb = createQueryBuilder(pageable, publishStatus)
         val query = createQuery("FindsAllByPublishStatus.countAllFilteredByPublishStatus()", nqb, LoggerFactory.getLogger(FindsAllByPublishStatus::class.java))
         return elasticSearchTemplate.count(query, javaClass)
-         */
-    }
-
-    @Deprecated("Upgrade to ES v8.x queries", ReplaceWith("createQueryBuilder"), DeprecationLevel.WARNING)
-    fun buildQuery(
-        pageable: Pageable,
-        publishStatus: Set<PublishStatus>
-    ): NativeSearchQueryBuilder {
-        val nsqb = NativeSearchQueryBuilder().withPageable(pageable)
-        nsqb.withQuery(QueryBuilders.matchAllQuery())
-        nsqb.withFilter(
-            BoolQueryBuilder().should(
-                QueryBuilders.termsQuery(
-                    "publishStatus",
-                    publishStatus.map { ps -> ps.toString() }
-                )
-            )
-        )
-        return nsqb
-    }
-
-    @Deprecated("", ReplaceWith("createQuery"), DeprecationLevel.WARNING)
-    fun createStringQuery(msgPrefix: String, nqb: NativeSearchQueryBuilder, log: Logger): Query {
-        val queryStr = nqb.build().query.toString()
-        log.debug(String.Companion.format("%s:\n%s", msgPrefix, queryStr))
-        return StringQuery(queryStr)
-    }
-
-    fun createQuery(msgPrefix: String, nqb: NativeQueryBuilder, log: Logger): Query {
-        val nq = nqb.build()
-        log.debug(String.Companion.format("%s:\n%s", msgPrefix, nq.query.toString()))
-        return nq;
     }
 
     fun createQueryBuilder(pageable: Pageable, publishStatus: Set<PublishStatus>): NativeQueryBuilder {
@@ -99,5 +45,18 @@ interface FindsAllByPublishStatus<T> {
             .withPageable(pageable)
             .withQuery(MATCH_ALL)
             .withFilter(filter)
+    }
+
+    fun createQuery(msgPrefix: String, nqb: NativeQueryBuilder, log: Logger): Query {
+        val nq = nqb.build()
+        log.debug(String.Companion.format("%s:\n%s", msgPrefix, nq.query.toString()))
+        return nq;
+    }
+
+    @Deprecated("", ReplaceWith("createQuery"), DeprecationLevel.WARNING)
+    fun createStringQuery(msgPrefix: String, nqb: NativeSearchQueryBuilder, log: Logger): Query {
+        val queryStr = nqb.build().query.toString()
+        log.debug(String.Companion.format("%s:\n%s", msgPrefix, queryStr))
+        return StringQuery(queryStr)
     }
 }
